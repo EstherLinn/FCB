6af6f10b3150e723b20bd3dcf115bf0189398ed6
Andrew Wu <andrew.wu@esi-tech.net>
2024-03-07 16:09:47 +0800

    fix

--- src/Feature/Component/platform/Controllers/JoinMemberInfoController.cs
+++ src/Feature/Component/platform/Controllers/JoinMemberInfoController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Feature.Weakth.Component.Models.JoinMemberInfo;
+using Feature.Wealth.Component.Models.JoinMemberInfo;
 using Sitecore.Data.Items;
 using Sitecore.Mvc.Presentation;
 
