a43da0688d8c53a9c16e2a5f82f1aa77a93e274a
Andrew Wu <andrew.wu@esi-tech.net>
2024-03-07 16:08:27 +0800

    Add JoinMemberInfo

---
+++ src/Feature/Component/platform/Controllers/JoinMemberInfoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Feature.Weakth.Component.Models.JoinMemberInfo;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+
+namespace Feature.Weakth.Component.Controllers
+{
+    public class JoinMemberInfoController : Controller
+    {
+        public ActionResult Index()
+        {
+            var dataSourceItem = RenderingContext.Current.Rendering.Item;
+
+            return View("/Views/Wealth/JoinMemberInfo/JoinMemberInfo.cshtml", CreateModel(dataSourceItem));
+        }
+
+        protected JoinMemberInfoModel CreateModel(Item dataSourceItem)
+        {
+            var model = new JoinMemberInfoModel();
+
+            model.Item = dataSourceItem;
+
+            return model;
+        }
+    }
+}
---
+++ src/Feature/Component/platform/Models/JoinMemberInfo/JoinMemberInfoModel.cs
@@ -0,0 +1,116 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Resources.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Feature.Wealth.Component.Models.JoinMemberInfo
+{
+    public class JoinMemberInfoModel
+    {
+        public Item Item { get; set; }
+
+        private MvcHtmlString GetFieldValue(string fieldName)
+        {
+            return new MvcHtmlString(Sitecore.Web.UI.WebControls.FieldRenderer.Render(Item, fieldName));
+        }
+
+        public MvcHtmlString Title
+        {
+            get { return GetFieldValue("Title"); }
+        }
+
+        public MvcHtmlString Subtitle
+        {
+            get { return GetFieldValue("Subtitle"); }
+        }
+
+        public MvcHtmlString Title1
+        {
+            get { return GetFieldValue("Titl1"); }
+        }
+
+        public MvcHtmlString Title2
+        {
+            get { return GetFieldValue("Titl2"); }
+        }
+
+        public MvcHtmlString Title3
+        {
+            get { return GetFieldValue("Titl3"); }
+        }
+
+        public MvcHtmlString Description1
+        {
+            get { return GetFieldValue("Description1"); }
+        }
+
+        public MvcHtmlString Description2
+        {
+            get { return GetFieldValue("Description2"); }
+        }
+
+        public MvcHtmlString Description3
+        {
+            get { return GetFieldValue("Description3"); }
+        }
+
+        private string GetImageUrl(string fieldName)
+        {
+            var imgField = (ImageField)Item.Fields[fieldName];
+            return imgField?.MediaItem != null ? MediaManager.GetMediaUrl(imgField.MediaItem) : string.Empty;
+        }
+
+        public string Image1
+        {
+            get
+            {
+                return GetImageUrl("Image1");
+            }
+        }
+
+        public string Image1_3X
+        {
+            get
+            {
+                return GetImageUrl("Image1_3X");
+            }
+        }
+
+        public string Image2
+        {
+            get
+            {
+                return GetImageUrl("Image2");
+            }
+        }
+
+        public string Image2_3X
+        {
+            get
+            {
+                return GetImageUrl("Image2_3X");
+            }
+        }
+
+        public string Image3
+        {
+            get
+            {
+                return GetImageUrl("Image3");
+            }
+        }
+
+        public string Image3_3X
+        {
+            get
+            {
+                return GetImageUrl("Image3_3X");
+            }
+        }
+    }
+}
