093c63a2139a68196c8ff8dcb551c6da45470014
kyle.wang <kyle.wang>
2024-03-11 17:31:39 +0000

    Update: 更新站台結構
    - 新增 Xcms.Sitecore.Foundation.Basic 套件
    - 新增 extensions.js
    - 新增切版
    - 更新 pubxml

--- src/Project/Wealth/platform/themes/js/extensions/highcharts-initialize.js
+++ src/Project/Wealth/platform/themes/js/extensions/highcharts-initialize.js
@@ -1,4 +1,4 @@
-const chartColors = ['#238C6C', '#D0A66C', '#F58220', '#A4844E', '#948BF2', '#FFAD04', '#FF77C0', '#C16FE8', '#DD7676', '#ABA1E4', '#86BDDD', '#60A6E2'];
+const chartColors = ['#238C6C', '#D0A66C', '#F58220', '#A4844E', '#948BF2', '#FFAD04', '#FF77C0', '#C16FE8', '#DD7676', '#ABA1E4', '#86BDDD', '#60A6E2', '#3AAB68', '#D0AD72', '#FDAA5E', '#63C98D', '#665DCC', '#E7465A'];
 function apiRequestGet(url, params) {
   return new Promise((resolve, reject) => {
     $.ajax({
