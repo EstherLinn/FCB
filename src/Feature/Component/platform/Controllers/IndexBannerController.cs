4ba0475d1ad8337ed36d693e45868fab6a871367
Esther Lin <esther.lin@esi-tech.net>
2024-03-14 19:12:53 +0800

    Add: B01 IndexBanner

---
+++ src/Feature/Component/platform/Controllers/IndexBannerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Feature.Wealth.Component.Models.IndexBanner;
+using Sitecore.Mvc.Presentation;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    /// <summary>
+    /// B01-首頁Banner元件
+    /// </summary>
+    public class IndexBannerController : Controller
+    {
+        public ActionResult Index()
+        {
+            var dataSourceItem = RenderingContext.Current.Rendering.Item;
+            var childItems = dataSourceItem.Children.ToList();
+
+            var model = new IndexBannerModel
+            {
+                DataSource = dataSourceItem,
+                Items = childItems.Select(item => new IndexBannerModel.Banner(item)).ToList()
+            };
+
+            return View("/Views/Feature/Wealth/Component/IndexBanner/IndexBanner.cshtml", model);
+        }
+    }
+}
---
+++ src/Feature/Component/platform/Models/IndexBanner/IndexBannerModel.cs
@@ -0,0 +1,51 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Resources.Media;
+using Sitecore.Links;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feature.Wealth.Component.Models.IndexBanner
+{
+    public class IndexBannerModel
+    {
+        public Item DataSource { get; set; }
+
+        public IList<Banner> Items { get; set; }
+
+        public class Banner
+        {
+            public Banner(Item item)
+            {
+                Item = item;
+            }
+
+            public Item Item { get; set; }
+            public string TitleField => Item["Title"];
+            public string SubtitleField => Item["Subtitle"];
+            public string ButtonText => Item["Button Text"];
+
+            public string ButtonLink
+            {
+                get
+                {
+                    var btnLink = ((LinkField)Item.Fields["Button Link"]);
+                    return btnLink.Url;
+                }
+            }
+
+
+            public string Image
+            {
+                get
+                {
+                    var imgField = (ImageField)Item.Fields["Image"];
+                    return imgField?.MediaItem != null ? MediaManager.GetMediaUrl(imgField.MediaItem) : string.Empty;
+                }
+            }
+        }
+    }
+}
