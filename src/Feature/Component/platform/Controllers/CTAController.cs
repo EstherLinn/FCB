5bdf5af684f93c39864b23f3562fd9d0f87d25c4
Esther Lin <esther.lin@esi-tech.net>
2024-03-15 10:17:13 +0800

    ADD: CTA

---
+++ src/Feature/Component/platform/Controllers/CTAController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Feature.Wealth.Component.Models.CTA;
+using Sitecore.Mvc.Presentation;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class CTAController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new CTAModel()
+            {
+                Item = RenderingContext.Current?.Rendering.Item
+            };
+            return View("/Views/Feature/Wealth/Component/CTA/CTA.cshtml", model);
+        }
+    }
+}
