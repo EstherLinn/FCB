e61f816c71c4e3663c57feaf9311cf2897e9b935
kyle.wang <kyle.wang>
2024-03-18 08:13:01 +0000

    Add: 新增前端 API 呼叫方法

---
+++ src/Foundation/Wealth/platform/Pipelines/Initialize/ClientRoute.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Xcms.Sitecore.Foundation.Basic.Pipelines;
+
+namespace Foundation.Wealth.Pipelines.Initialize
+{
+    public class ClientRoute : RegisterMvcRouteBase
+    {
+        public override string RouteName => nameof(ClientRoute);
+
+        protected override void Register(RouteCollection routes)
+        {
+            routes.MapRoute(this.RouteName, "api/client/{controller}/{action}", new { param = UrlParameter.Optional });
+        }
+    }
+}
