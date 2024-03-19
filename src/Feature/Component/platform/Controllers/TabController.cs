09a4518ccd7fd8d2b18a0946babd68aa9656b024
Rosie <rosie.chiu@esi-tech.net>
2024-03-19 11:05:32 +0800

    Add: Tab元件

---
+++ src/Feature/Component/platform/Controllers/TabController.cs
@@ -0,0 +1,30 @@
+﻿using Feature.Wealth.Component.Models.Tab;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Web.Mvc;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class TabController : Controller
+    {
+        public ActionResult Index()
+        {
+            Item datasource = null;
+
+            if (Guid.TryParse(RenderingContext.Current.Rendering.DataSource, out Guid guid) && ID.TryParse(guid, out ID id))
+            {
+                datasource = Sitecore.Context.Database.GetItem(id);
+            }
+
+            return View("/Views/Feature/Wealth/Component/Tab/Tab.cshtml", CreateModel(datasource));
+        }
+
+        protected TabModel CreateModel(Item item)
+        {
+            var model = new TabModel(item);
+            return model;
+        }
+    }
+}
---
+++ src/Feature/Component/platform/Models/Tab/TabModel.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Feature.Wealth.Component.Models.Tab
+{
+    public class TabModel
+    {
+        public Item Datasource { get; private set; }
+
+        public IList<Item> SubItems { get; private set; }
+
+        public TabModel(Item item)
+        {
+            this.Datasource = item;
+            this.SubItems = this.Datasource?.GetChildren()?.Where(i => i.Versions?.Count > 0)?.ToList() ?? new List<Item>();
+        }
+    }
+
+    public static class DatasourceTemplates
+    {
+        public static readonly ID MainTitle = new ID("{94C4C08F-E9FD-43B5-8345-C67CAE820F85}");
+    }
+
+    public static class SubItemTemplates
+    {
+        public static readonly ID Title = new ID("{6BCC6916-B2DD-40C1-A956-D538AF611F49}");
+    }
+}
