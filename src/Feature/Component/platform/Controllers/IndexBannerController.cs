4565188fe06be3b7d4221d7698121a80ea0a1305
Esther Lin <esther.lin@esi-tech.net>
2024-03-19 20:29:11 +0800

    Update: 改使用ItemUtils

--- src/Feature/Component/platform/Controllers/IndexBannerController.cs
+++ src/Feature/Component/platform/Controllers/IndexBannerController.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Web.Mvc;
 using Feature.Wealth.Component.Models.IndexBanner;
-using Sitecore.Data.Fields;
 using Sitecore.Mvc.Presentation;
 using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
 
@@ -16,17 +15,14 @@ namespace Feature.Wealth.Component.Controllers
         public ActionResult Index()
         {
             var dataSourceItem = RenderingContext.CurrentOrNull?.Rendering.Item;
-            var childItems = dataSourceItem.Children.ToList();
+            var childItems = ItemUtils.GetChildren(dataSourceItem).ToList();
 
             var items = new List<IndexBannerModel.Banner>();
 
             foreach (var childItem in childItems)
             {
-                var imageField = (ImageField)childItem.Fields[IndexBannerModel.Banner.Image];
-                string imageUrl = imageField != null ? FieldUtils.ImageUrl(imageField) : string.Empty;
-
-                var btnField = childItem.Fields[IndexBannerModel.Banner.ButtonLink];
-                var btnLink = btnField != null ? FieldUtils.GeneralLink(btnField).Url : string.Empty;
+                var imageUrl = ItemUtils.ImageUrl(childItem, IndexBannerModel.Banner.Image);
+                var btnLink = ItemUtils.GeneralLink(childItem, IndexBannerModel.Banner.ButtonLink).Url;
 
                 items.Add(new IndexBannerModel.Banner(childItem)
                 {
