a0def7dc229cfeaf58b4dee2d8e065b344029829
m29311902 <77109325+m29311902@users.noreply.github.com>
2024-03-20 19:01:23 +0800

    20240320 Add: Footer

---
+++ src/Feature/Component/platform/Controllers/BankController.cs
@@ -0,0 +1,13 @@
+﻿using Feature.Wealth.Component.Repositories;
+using Sitecore.Mvc.Controllers;
+using System.Web.Mvc;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class BankController : SitecoreController
+    {
+        private readonly BankRepository _bankRepository = new();
+
+        public ActionResult Footer() => View("~/Views/Feature/Wealth/Component/Footer/Footer.cshtml", this._bankRepository.GetFooter());
+    }
+}
---
+++ src/Feature/Component/platform/Models/Footer/FooterModel.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Data.Items;
+using System.Collections.Generic;
+
+namespace Feature.Wealth.Component.Models.Footer
+{
+    public class FooterModel
+    {
+        public Item DataSource { get; set; }
+        public IList<FooterSocialLinkItem> LeftLinkItems { get; set; }
+        public IList<FooterLinkGroup> RightLinkItems { get; set; }   
+    }
+
+    public class FooterLinkGroup
+    {
+        public Item Item { get; set; }
+        public IList<FooterLinkItem> Children { get; set; }
+    }
+
+    public class FooterLinkItem
+    {
+        public Item Item { get; set; }
+    }
+
+    public class FooterSocialLinkItem
+    {
+        public Item Item { get; set; }
+    }
+}
