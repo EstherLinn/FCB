b226cadc083b35a0ebfd52c0ee31a70815e94a74
Cheryl Chang <cheryl.chang@esi-tech.net>
2024-03-21 18:50:09 +0800

    0321

---
+++ src/Feature/Component/platform/Controllers/BannerController.cs
@@ -0,0 +1,24 @@
+﻿using Feature.Wealth.Component.Models.Banner;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class BannerController : Controller
+    {
+        public ActionResult InnerBanner()
+        {
+            var model = new InnerBannerModel()
+            {
+                Item = RenderingContext.Current?.Rendering.Item
+            };
+            return View("/Views/Feature/Component/Banner/InnerBanner.cshtml", model);
+        }
+    }
+
+}
---
+++ src/Feature/Component/platform/Models/Banner/InnerBannerModel.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feature.Wealth.Component.Models.Banner
+{
+    public class InnerBannerModel
+    {
+        public Item Item { get; set; }
+    }
+
+    public struct Template
+    {
+        /// <summary>
+        /// 圖片
+        /// </summary>
+        public static readonly string Image = "{8C374ADE-0694-4757-B8ED-D3984F2C947E}";
+
+        /// <summary>
+        /// 標題
+        /// </summary>
+        public static readonly string Title = "{397AF03E-2E2C-4AD1-A2BD-327C1CF047C4}";
+
+    }
+
+}
+
