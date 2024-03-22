724b01cbc6ceb7eb550a6749a975996f1ff13a93
Cheryl Chang <cheryl.chang@esi-tech.net>
2024-03-22 15:32:22 +0800

    0322

--- src/Feature/Component/platform/Controllers/BannerController.cs
+++ src/Feature/Component/platform/Controllers/BannerController.cs
@@ -1,10 +1,5 @@
 ﻿using Feature.Wealth.Component.Models.Banner;
 using Sitecore.Mvc.Presentation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Mvc;
 using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
 
