4930cc6a5587ebea89a83974d3b2fd639dd70d82
Esther Lin <esther.lin@esi-tech.net>
2024-03-22 18:26:03 +0800

    ADD: 熱銷基金排行

---
+++ src/Feature/Component/platform/Controllers/HotFundController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Feature.Wealth.Component.Models.HotFund;
+using Feature.Wealth.Component.Repositories;
+using Sitecore.Mvc.Presentation;
+using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
+using static Feature.Wealth.Component.Models.HotFund.HotFundModel;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class HotFundController : Controller
+    {
+        private HotFundRepository _HotFundRepository = new HotFundRepository();
+
+        public ActionResult Index()
+        {
+            var dataSourceItem = RenderingContext.CurrentOrNull.Rendering.Item;
+            var multilineField = ItemUtils.GetMultiLineText(dataSourceItem, HotFundModel.Template.HotFund.Fields.FundID);
+            var viewModel = new HotFundModel { Item = dataSourceItem };
+            if (multilineField != null)
+            {
+                viewModel.selectedId = multilineField;
+                var funds = _HotFundRepository.GetFundData();
+                var hotFunds = funds.Where(fund => multilineField.Contains(fund.ProductCode)).ToList();
+                hotFunds = hotFunds.OrderByDescending(f => f.SixMonthReturnOriginalCurrency).ToList();
+                viewModel.HotFunds = _HotFundRepository.GetFundRenderData(hotFunds);
+            }
+            return View("/Views/Feature/Wealth/Component/HotFund/HotFund.cshtml", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult GetSortedHotFund(string[] selectedId, string orderby = "SixMonthReturnOriginalCurrency", string desc = "is-desc")
+        {
+            var funds = _HotFundRepository.GetFundData();
+            var hotFunds = funds.Where(fund => selectedId.Contains(fund.ProductCode)).ToList();
+
+            var property = typeof(Funds).GetProperty(orderby);
+            if (desc == "is-desc")
+            {
+                hotFunds = hotFunds.OrderByDescending(f => property.GetValue(f, null)).ToList();
+            }
+            else
+            {
+                hotFunds = hotFunds.OrderBy(f => property.GetValue(f, null)).ToList();
+            }
+
+            var renderDatas = _HotFundRepository.GetFundRenderData(hotFunds);
+
+            var viewModel = new HotFundModel
+            {
+                HotFunds = renderDatas
+            };
+            return View("/Views/Feature/Wealth/Component/HotFund/HotFundReturnView.cshtml", viewModel);
+        }
+
+
+    }
+}
---
+++ src/Feature/Component/platform/Repositories/HotFundRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using Foundation.Wealth.Manager;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using static Feature.Wealth.Component.Models.HotFund.HotFundModel;
+
+namespace Feature.Wealth.Component.Repositories
+{
+    public class HotFundRepository
+    {
+        private readonly IDbConnection _dbConnection = DbManager.Custom.DbConnection();
+
+        public List<Funds> GetFundData()
+        {
+            List<Funds> fundItems = new List<Funds>();
+
+            string sql = @"SELECT * FROM [FCB_sitecore_Custom].[dbo].[vw_BasicFund]";
+            var results = _dbConnection.Query<Funds>(sql).ToList();
+
+            foreach (var item in results)
+            {
+                ProcessFundFilterDatas(item);
+                fundItems.Add(item);
+            }
+            return fundItems;
+
+        }
+
+        private void ProcessFundFilterDatas(Funds item)
+        {
+            DateTime navdate = Convert.ToDateTime(item.NetAssetValueDate);
+            string formattedDate = navdate.ToString("yyyy-MM-dd");
+            item.NetAssetValueDate = formattedDate;
+            item.SixMonthReturnOriginalCurrency = decimal.Round(item.SixMonthReturnOriginalCurrency, 2);
+            item.NetAssetValue = decimal.Round(item.NetAssetValue, 4);
+            item.PercentageChangeInFundPrice = decimal.Round((item.PercentageChangeInFundPrice * 100), 4);
+        }
+
+        /// <summary>
+        /// 取得資料-列表渲染用
+        /// </summary>
+        public List<Funds> GetFundRenderData(List<Funds> funds)
+        {
+            var result = new List<Funds>();
+
+            foreach (var f in funds)
+            {
+                var vm = new Funds();
+                vm.ProductCode = f.ProductCode;
+                vm.ProductName = f.ProductName;
+                vm.NetAssetValue = f.NetAssetValue;
+                vm.NetAssetValueDate = f.NetAssetValueDate;
+                vm.SixMonthReturnOriginalCurrency = f.SixMonthReturnOriginalCurrency;
+                vm.ValuationCurrency = f.ValuationCurrency;
+                vm.RiskRewardLevel = f.RiskRewardLevel;
+                vm.OnlineSubscriptionAvailability = f.OnlineSubscriptionAvailability;
+                vm.PercentageChangeInFundPrice = f.PercentageChangeInFundPrice;
+                vm.DomesticInvestmentRatio = f.DomesticInvestmentRatio;
+                vm.TargetName = f.TargetName;
+                result.Add(vm);
+            }
+            return result;
+        }
+    }
+}
