4ad8bdd27964374c4839d45a08d0d6dfefe81748
Cheryl Chang <cheryl.chang@esi-tech.net>
2024-03-22 15:17:54 +0800

    0322

--- src/Feature/Component/platform/Models/Banner/InnerBannerModel.cs
+++ src/Feature/Component/platform/Models/Banner/InnerBannerModel.cs
@@ -13,12 +13,12 @@ namespace Feature.Wealth.Component.Models.Banner
         /// <summary>
         /// 圖片
         /// </summary>
-        public static readonly string Image = "{8C374ADE-0694-4757-B8ED-D3984F2C947E}";
+        public static readonly string Image = "{D17D168A-1950-426A-B8BC-21ACC3AC3B9B}";
 
         /// <summary>
         /// 標題
         /// </summary>
-        public static readonly string Title = "{397AF03E-2E2C-4AD1-A2BD-327C1CF047C4}";
+        public static readonly string Title = "{27B232AB-8B31-4E68-8E3D-D0F7B55D8ADB}";
 
     }
 }
