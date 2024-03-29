b6e2ce29f6d47f87b4da0e0012db528768fa702e
Esther Lin <esther.lin@esi-tech.net>
2024-03-29 13:30:36 +0800

    Update: PCMB圖片、深色模式、按鈕防呆、Model大寫

--- src/Feature/Component/platform/Controllers/IndexBannerController.cs
+++ src/Feature/Component/platform/Controllers/IndexBannerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Sitecore.Mvc.Presentation;
 using Feature.Wealth.Component.Models.IndexBanner;
 using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
+using static Feature.Wealth.Component.Models.IndexBanner.IndexBannerModel;
 
 namespace Feature.Wealth.Component.Controllers
 {
@@ -16,17 +17,23 @@ namespace Feature.Wealth.Component.Controllers
             var dataSourceItem = RenderingContext.CurrentOrNull?.Rendering.Item;
             var childItems = ItemUtils.GetChildren(dataSourceItem);
 
-            var items = new List<IndexBannerModel.Banner>();
+            var items = new List<Banner>();
 
             foreach (var childItem in childItems)
             {
-                string imageUrl = ItemUtils.ImageUrl(childItem, IndexBannerModel.Banner.Template.Fields.Image);
-                string btnLink = ItemUtils.GeneralLink(childItem, IndexBannerModel.Banner.Template.Fields.ButtonLink)?.Url;
+                string imageUrlPC = ItemUtils.ImageUrl(childItem, Template.IndexBanner.Fields.ImagePC);
+                string imageUrlMB = ItemUtils.ImageUrl(childItem, Template.IndexBanner.Fields.ImageMB);
+                string btnLink = ItemUtils.GeneralLink(childItem, Template.IndexBanner.Fields.ButtonLink)?.Url;
+                bool checkedDarkMode = ItemUtils.IsChecked(childItem, Template.IndexBanner.Fields.DarkMode);
+                string btnText = ItemUtils.GetFieldValue(childItem, Template.IndexBanner.Fields.ButtonText);
 
-                items.Add(new IndexBannerModel.Banner(childItem)
+                items.Add(new Banner(childItem)
                 {
-                    imageUrl = imageUrl,
-                    btnLink = btnLink
+                    IsDark = checkedDarkMode,
+                    ImageUrlPC = imageUrlPC,
+                    ImageUrlMB = imageUrlMB,
+                    ButtonLink = btnLink,
+                    ButtonText = btnText
                 });
             }
 
--- src/Feature/Component/platform/Models/IndexBanner/IndexBannerModel.cs
+++ src/Feature/Component/platform/Models/IndexBanner/IndexBannerModel.cs
@@ -18,41 +18,57 @@ namespace Feature.Wealth.Component.Models.IndexBanner
             }
 
             public Item Item { get; set; }
-            public string imageUrl { get; set; }
-            public string btnLink { get; set; }
+            public bool IsDark { get; set; }
+            public string ImageUrlPC { get; set; }
+            public string ImageUrlMB { get; set; }
+            public string ButtonLink { get; set; }
+            public string ButtonText { get; set; }
+        }
+    }
 
 
-            public struct Template
+    public struct Template
+    {
+        public struct IndexBanner
+        {
+            public static readonly ID Id = new ID("{42237DF0-FA9A-4CA5-A848-0D41E244EE3F}");
+
+            public struct Fields
             {
-                public static readonly ID Id = new ID("{42237DF0-FA9A-4CA5-A848-0D41E244EE3F}");
+                /// <summary>
+                /// 主標題
+                /// </summary>
+                public static readonly ID Title = new ID("{F2D070E9-73F1-4093-BA86-65F54FDA8F91}");
 
-                public struct Fields
-                {
-                    /// <summary>
-                    /// 主標題
-                    /// </summary>
-                    public static readonly ID Title = new ID("{F2D070E9-73F1-4093-BA86-65F54FDA8F91}");
+                /// <summary>
+                /// 次標題
+                /// </summary>
+                public static readonly ID Subtitle = new ID("{1A558A9E-E7E7-414B-8051-CD440E7D68A2}");
 
-                    /// <summary>
-                    /// 次標題
-                    /// </summary>
-                    public static readonly ID Subtitle = new ID("{1A558A9E-E7E7-414B-8051-CD440E7D68A2}");
+                /// <summary>
+                /// 是否為暗黑模式
+                /// </summary>
+                public static readonly ID DarkMode = new ID("{FDC136D5-0AD4-47E0-B483-24637AD2F982}");
 
-                    /// <summary>
-                    /// 圖片
-                    /// </summary>
-                    public static readonly ID Image = new ID("{1412E584-BB02-4848-92E0-3E68EC670E16}");
+                /// <summary>
+                /// 圖片-電腦版
+                /// </summary>
+                public static readonly ID ImagePC = new ID("{1412E584-BB02-4848-92E0-3E68EC670E16}");
 
-                    /// <summary>
-                    /// 按鈕連結文字
-                    /// </summary>
-                    public static readonly ID ButtonText = new ID("{EC121B56-E4A5-471B-8288-9BB0E10D62CB}");
+                /// <summary>
+                /// 圖片-手機板
+                /// </summary>
+                public static readonly ID ImageMB = new ID("{D8422A07-DDCC-4589-985C-7595C110C478}");
 
-                    /// <summary>
-                    /// 按鈕連結
-                    /// </summary>
-                    public static readonly ID ButtonLink = new ID("{A876C9CA-B633-4794-BA01-6665D96E1627}");
-                }
+                /// <summary>
+                /// 按鈕連結文字
+                /// </summary>
+                public static readonly ID ButtonText = new ID("{EC121B56-E4A5-471B-8288-9BB0E10D62CB}");
+
+                /// <summary>
+                /// 按鈕連結
+                /// </summary>
+                public static readonly ID ButtonLink = new ID("{A876C9CA-B633-4794-BA01-6665D96E1627}");
             }
         }
     }
