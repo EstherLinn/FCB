2dcbd4f08088dfdc3064c52f9dba78d75a1f5ac3
Michael Lee <michael.lee@esi-tech.net>
2024-03-29 17:35:34 +0800

    Merge 'feature/Michael/#D05_DiscountTab' into 'developer'

---
+++ src/Feature/Component/platform/Controllers/DiscountTabController.cs
@@ -0,0 +1,29 @@
+﻿using Feature.Wealth.Component.Models.DiscountTab;
+using Sitecore.Mvc.Presentation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class DiscountTabController : Controller
+    {
+        public ActionResult Index()
+        {
+            var dataSource = RenderingContext.CurrentOrNull?.Rendering.Item;
+
+            IEnumerable<DiscountTabItem> tabList = dataSource.GetChildren(Templates.DiscountTabItem.ID)
+            .Where(children => dataSource.GetChildren(Templates.DiscountTabItem.ID).Any())
+            .Select(x => new DiscountTabItem { Item = x });
+
+            var model = new DiscountTabModel()
+            {
+                DataSource = dataSource,
+                TabList = tabList
+            };
+
+            return View("/Views/Feature/Wealth/Component/DiscountTab/DiscountTab.cshtml", model);
+        }
+    }
+}
