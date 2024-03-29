ce84c2604930f44f8293f239c1dbe6a59533dba6
Cheryl Chang <cheryl.chang@esi-tech.net>
2024-03-29 17:34:42 +0800

    Add: D03 注意事項收闔元件 Add: D09 大注意事項元件

---
+++ src/Feature/Component/platform/Controllers/NoticeController.cs
@@ -0,0 +1,29 @@
+﻿using Feature.Wealth.Component.Models.Notice;
+using Sitecore.Mvc.Presentation;
+using System.Web.Mvc;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class NoticeController : Controller
+    {
+        public ActionResult Notice()
+        {
+            var model = new NoticeModel()
+            {
+                Item = RenderingContext.CurrentOrNull?.Rendering.Item
+            };
+
+            return View("/Views/Feature/Wealth/Component/Notice/Notice.cshtml", model);
+        }
+
+        public ActionResult BigNotice()
+        {
+            var model = new NoticeModel()
+            {
+                Item = RenderingContext.CurrentOrNull?.Rendering.Item
+            };
+
+            return View("/Views/Feature/Wealth/Component/Notice/BigNotice.cshtml", model);
+        }
+    }
+}
---
+++ src/Feature/Component/platform/Models/Notice/NoticeModel.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+
+namespace Feature.Wealth.Component.Models.Notice
+{
+    public class NoticeModel
+    {
+        public Item Item { get; set; }
+    }
+
+    public struct Templates
+    {
+        public struct Notice
+        {
+            public static readonly ID Id = new ID("{92C6CB62-2192-434B-8713-C4C199AAEB86}");
+
+            public struct Fields
+            {
+                /// <summary>
+                /// 主標題
+                /// </summary>
+                public static readonly ID MainTitle = new ID("{C4A879F2-CFCF-4095-A0EB-57AF981E71FB}");
+
+                /// <summary>
+                /// 內容
+                /// </summary>
+                public static readonly ID Content = new ID("{59945339-D1E2-447B-ADAD-5D3E1BBB99C5}");
+            }
+        }
+    }
+}
