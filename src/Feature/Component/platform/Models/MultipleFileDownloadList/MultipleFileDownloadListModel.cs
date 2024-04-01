7fca4af49c42e736517d527c654ccd6fe76cc2af
Michael Lee <michael.lee@esi-tech.net>
2024-04-01 16:58:36 +0800

    Merge 'feature/Michael/#D12_MultipleFileDownloadList' into 'developer'

---
+++ src/Feature/Component/platform/Models/MultipleFileDownloadList/MultipleFileDownloadListModel.cs
@@ -0,0 +1,57 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using System.Collections.Generic;
+
+namespace Feature.Wealth.Component.Models.MultipleFileDownloadList
+{
+    public class MultipleFileDownloadListModel
+    {
+        public Item DataSource { get; set; }
+        public IEnumerable<DownloadListItemModel> DownloadListItems { get; set; }
+    }
+
+    public class DownloadListItemModel
+    {
+        public Item item { get; set; }
+        public string ItemTitle { get; set; }
+        public IEnumerable<DownloadListSubItemModel> DownloadListSubitems { get; set; }
+    }
+
+    public class DownloadListSubItemModel
+    {
+        public Item item { get; set; }
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public string Link { get; set; }
+    }
+
+    public static class Templates
+    {
+        public struct MultipleFileDownloadListDatasource
+        {
+            public static readonly ID Id = new ID("{5B184CFE-CA83-45FE-9938-30DA2B297958}");
+        }
+
+        public struct MultipleFileDownloadListItem
+        {
+            public static readonly ID Id = new ID("{0934266B-DFE2-4F5E-A2A4-FC92E872C5F7}");
+
+            public struct Fields
+            {
+                internal static readonly ID ItemTitle = new ID("{3893A333-FEEF-441F-95BB-EF7BA102BBE9}");
+            }
+        }
+
+        public struct MultipleFileDownloadListSubItem
+        {
+            public static readonly ID Id = new ID("{D89325A3-6D40-4C8B-8AEA-12B60EE5084F}");
+
+            public struct Fields
+            {
+                internal static readonly ID Title = new ID("{93DACEE1-8900-4F57-BF7B-95292EE029C9}");
+                internal static readonly ID Date = new ID("{3F223052-1FF6-40A9-8AF7-889575B17283}");
+                internal static readonly ID Link = new ID("{CD8C6328-CF0C-4814-A888-77A495E202B3}");
+            }
+        }
+    }
+}
---
+++ src/Feature/Component/platform/Repositories/MultipleFileDownloadListRepository.cs
@@ -0,0 +1,56 @@
+﻿using Feature.Wealth.Component.Models.MultipleFileDownloadList;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
+
+namespace Feature.Wealth.Component.Repositories
+{
+    public class MultipleFileDownloadListRepository
+    {
+        public MultipleFileDownloadListModel GetMultipleFileDownloadList()
+        {
+            var dataSource = RenderingContext.CurrentOrNull?.Rendering?.Item;
+            if (dataSource == null)
+            {
+                return null;
+            }
+
+            var listItems = new List<DownloadListItemModel>();
+
+            foreach (var listItem in dataSource.Children.Where(child => child.TemplateID == Templates.MultipleFileDownloadListItem.Id))
+            {
+                var listSubItems = new List<DownloadListSubItemModel>();
+
+                foreach (var listSubItem in listItem.Children.Where(child => child.TemplateID == Templates.MultipleFileDownloadListSubItem.Id))
+                {
+                    DateTime date = (DateTime)ItemUtils.GetDateTimeFieldValue(listSubItem, Templates.MultipleFileDownloadListSubItem.Fields.Date);
+
+                    listSubItems.Add(new DownloadListSubItemModel
+                    {
+                        item = listSubItem,
+                        Date = date != DateTime.MinValue ? date.ToString("yyyy/MM/dd") : string.Empty,
+                        Title = ItemUtils.GetFieldValue(listSubItem, Templates.MultipleFileDownloadListSubItem.Fields.Title),
+                        Link = ItemUtils.GeneralLink(listSubItem, Templates.MultipleFileDownloadListSubItem.Fields.Link).Url
+                    });
+                }
+
+                listItems.Add(new DownloadListItemModel
+                {
+                    item = listItem,
+                    ItemTitle = ItemUtils.GetFieldValue(listItem, Templates.MultipleFileDownloadListItem.Fields.ItemTitle),
+                    DownloadListSubitems = listSubItems
+                });
+            }
+
+            var downloadList = new MultipleFileDownloadListModel
+            {
+                DataSource = dataSource,
+                DownloadListItems = listItems
+            };
+
+            return downloadList;
+        }
+    }
+}
