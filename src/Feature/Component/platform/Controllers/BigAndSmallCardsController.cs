55efec20e003183e947c14dbf2aaf2a3ec8f6978
Cheryl Chang <cheryl.chang@esi-tech.net>
2024-04-10 12:04:56 +0800

    Add: D27.一大三小牌卡元件

---
+++ src/Feature/Component/platform/Controllers/BigAndSmallCardsController.cs
@@ -0,0 +1,46 @@
+﻿using Feature.Wealth.Component.Models.BigAndSmallCards;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class BigAndSmallCardsController : Controller
+    {
+        public ActionResult Index()
+        {
+            var item = RenderingContext.CurrentOrNull?.Rendering.Item;
+
+            var imageUrl1 = ItemUtils.ImageUrl(item, Templates.BigAndSmallCards.Fields.Image1);
+            var imageUrl2 = ItemUtils.ImageUrl(item, Templates.BigAndSmallCards.Fields.Image2);
+            var imageUrl3 = ItemUtils.ImageUrl(item, Templates.BigAndSmallCards.Fields.Image3);
+            var buttonLink1 = ItemUtils.GeneralLink(item, Templates.BigAndSmallCards.Fields.ButtonLink1)?.Url;
+            var buttonLink2 = ItemUtils.GeneralLink(item, Templates.BigAndSmallCards.Fields.ButtonLink2)?.Url;
+            var buttonLink3 = ItemUtils.GeneralLink(item, Templates.BigAndSmallCards.Fields.ButtonLink3)?.Url;
+            var bigImageUrl = ItemUtils.ImageUrl(item, Templates.BigAndSmallCards.Fields.BigImage);
+            var bigButtonLink1 = ItemUtils.GeneralLink(item, Templates.BigAndSmallCards.Fields.BigButtonLink1)?.Url;
+            var bigButtonLink2 = ItemUtils.GeneralLink(item, Templates.BigAndSmallCards.Fields.BigButtonLink2)?.Url;
+
+            var model = new BigAndSmallCardsModel()
+            {
+                Item = item,
+                ImageUrl1 = imageUrl1,
+                ImageUrl2 = imageUrl2,
+                ImageUrl3 = imageUrl3,
+                ButtonLink1 = buttonLink1,
+                ButtonLink2 = buttonLink2,
+                ButtonLink3 = buttonLink3,
+                BigImageUrl = bigImageUrl,
+                BigButtonLink1 = bigButtonLink1,
+                BigButtonLink2 = bigButtonLink2,
+            };
+
+            return View("/Views/Feature/Wealth/Component/BigAndSmallCards/BigAndSmallCards.cshtml", model);
+        }
+    }
+}
