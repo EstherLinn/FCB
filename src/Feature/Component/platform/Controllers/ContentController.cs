8d1847718bed54f4016b7558654d1245f42783f4
Cheryl Chang <cheryl.chang@esi-tech.net>
2024-04-10 17:19:48 +0800

    Add: D29.一般內文元件

---
+++ src/Feature/Component/platform/Controllers/ContentController.cs
@@ -0,0 +1,19 @@
+﻿using Feature.Wealth.Component.Models.Notice;
+using Sitecore.Mvc.Presentation;
+using System.Web.Mvc;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class ContentController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new NoticeModel()
+            {
+                Item = RenderingContext.CurrentOrNull?.Rendering.Item
+            };
+
+            return View("/Views/Feature/Wealth/Component/Content/Content.cshtml", model);
+        }
+    }
+}
