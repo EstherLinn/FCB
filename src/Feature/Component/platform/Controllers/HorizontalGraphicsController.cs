8118d2bfa0a43d65245010f902486e78a3153fa2
Cheryl Chang <cheryl.chang@esi-tech.net>
2024-04-11 14:55:53 +0800

    Add: D24.左右圖文元件

---
+++ src/Feature/Component/platform/Controllers/HorizontalGraphicsController.cs
@@ -0,0 +1,38 @@
+﻿using Feature.Wealth.Component.Models.HorizontalGraphics;
+using Sitecore.Mvc.Presentation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class HorizontalGraphicsController : Controller
+    {
+        public ActionResult Index()
+        {
+            var dataSourceItem = RenderingContext.CurrentOrNull?.Rendering.Item;
+            var childItems = ItemUtils.GetChildren(dataSourceItem).ToList();
+
+            var items = new List<HorizontalGraphicsModel.HorizontalGraphics>();
+
+            foreach (var childItem in childItems)
+            {
+                var image = ItemUtils.ImageUrl(childItem, Templates.HorizontalGraphics.Fields.Image);
+
+                items.Add(new HorizontalGraphicsModel.HorizontalGraphics(childItem)
+                {
+                    ImageUrl = image,
+                });
+            }
+
+            var model = new HorizontalGraphicsModel
+            {
+                DataSource = dataSourceItem,
+                Items = items
+            };
+
+            return View("/Views/Feature/Wealth/Component/HorizontalGraphics/HorizontalGraphics.cshtml", model);
+        }
+    }
+}
---
+++ src/Feature/Component/platform/Models/HorizontalGraphics/HorizontalGraphicsModel.cs
@@ -0,0 +1,72 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using System.Collections.Generic;
+
+namespace Feature.Wealth.Component.Models.HorizontalGraphics
+{
+    public class HorizontalGraphicsModel
+    {
+        public Item DataSource { get; set; }
+        public Item Direction { get; set; }
+        public IList<HorizontalGraphics> Items { get; set; }
+
+        public class HorizontalGraphics
+        {
+            public HorizontalGraphics(Item item)
+            {
+                Item = item;
+            }
+            public Item Item { get; set; }
+            public string ImageUrl { get; set; }
+        }
+    }
+
+    public struct Templates
+    {
+        public struct HorizontalGraphicsTitle
+        {
+            public static readonly ID Id = new ID("{9BC33BE6-586F-49B2-A53F-47FE9277CDB9}");
+
+            public struct Fields
+            {
+                /// <summary>
+                /// 主標題
+                /// </summary>
+                public static readonly ID MainTitle = new ID("{4EA0DEA8-2A7E-4A27-8C4C-124C283B234C}");
+
+                /// <summary>
+                /// 描述
+                /// </summary>
+                public static readonly ID Description = new ID("{2DE8D708-D763-4B37-A8FE-8A2BE0491605}");
+
+                /// <summary>
+                /// 圖片方向
+                /// </summary>
+                public static readonly ID FadeDirection = new ID("{3BAAB9CB-12E6-413D-9374-3591D204865E}");
+            }
+        }
+
+        public struct HorizontalGraphics
+        {
+            public static readonly ID Id = new ID("{759CA276-16B9-4325-BD65-EE852B331231}");
+
+            public struct Fields
+            {
+                /// <summary>
+                /// 標題
+                /// </summary>
+                public static readonly ID Title = new ID("{20008C0F-AA09-478D-9B23-447BC947971D}");
+
+                /// <summary>
+                /// 內容
+                /// </summary>
+                public static readonly ID Content = new ID("{A8B64596-06EB-4EBF-945B-C7759023C509}");
+
+                /// <summary>
+                /// 圖片
+                /// </summary>
+                public static readonly ID Image = new ID("{5C02B39E-764B-448D-9803-4091EBC764BE}");
+            }
+        }
+    }
+}
