ac38d4b74f7b5b92a6a13608521009cfc9460983
Jason Hsieh <jason.hsieh@esi-tech.net>
2024-04-12 14:53:07 +0800

    add : D30.常見問題元件

---
+++ src/Feature/Component/platform/Controllers/CommonQuestionsController.cs
@@ -0,0 +1,15 @@
+﻿using Feature.Wealth.Component.Models.CommonQuestions;
+using Sitecore.Mvc.Presentation;
+using System.Web.Mvc;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class CommonQuestionsController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = RenderingContext.CurrentOrNull?.Rendering.Item;
+            return View("~/Views/Feature/Wealth/Component/CommonQuestions/CommonQuestions.cshtml", new CommonQuestionsModel(model));
+        }
+    }
+}
