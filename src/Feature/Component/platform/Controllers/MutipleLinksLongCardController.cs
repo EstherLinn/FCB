a09a2d137ad61f76081ab545e926daaa268fe969
Jason Hsieh <jason.hsieh@esi-tech.net>
2024-04-12 17:11:38 +0800

    add : D31.多連結長牌卡元件

---
+++ src/Feature/Component/platform/Controllers/MutipleLinksLongCardController.cs
@@ -0,0 +1,15 @@
+﻿using Feature.Wealth.Component.Models.MutipleLinksLongCard;
+using Sitecore.Mvc.Presentation;
+using System.Web.Mvc;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class MutipleLinksLongCardController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new MutipleLinksLongCardModel(RenderingContext.CurrentOrNull?.Rendering.Item);
+            return View("/Views/Feature/Wealth/Component/MutipleLinksLongCard/MutipleLinksLongCard.cshtml", model);
+        }
+    }
+}
