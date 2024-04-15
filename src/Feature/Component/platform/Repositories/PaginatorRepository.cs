53ef15dceeb67a4eda7d256c475f447d5ac5c2e2
Michael Lee <michael.lee@esi-tech.net>
2024-04-15 15:11:40 +0800

    Merge 'feature/Michael/#D20_Paginator' into 'developer'

---
+++ src/Feature/Component/platform/Repositories/PaginatorRepository.cs
@@ -0,0 +1,57 @@
+﻿using Sitecore.Data.Items;
+
+namespace Feature.Wealth.Component.Repositories
+{
+    public class PaginatorRepository
+    {
+        public Item GetPreviousPageItem(Item currentItem)
+        {
+            if (currentItem?.Parent != null)
+            {
+                Item[] siblings = currentItem.Parent.GetChildren().ToArray();
+
+                int currentIndex = -1;
+                for (int i = 0; i < siblings.Length; i++)
+                {
+                    if (siblings[i].ID == currentItem.ID)
+                    {
+                        currentIndex = i;
+                        break;
+                    }
+                }
+
+                if (currentIndex > 0)
+                {
+                    return siblings[currentIndex - 1];
+                }
+            }
+
+            return null;
+        }
+
+        public Item GetNextPageItem(Item currentItem)
+        {
+            if (currentItem?.Parent != null)
+            {
+                Item[] siblings = currentItem.Parent.GetChildren().ToArray();
+
+                int currentIndex = -1;
+                for (int i = 0; i < siblings.Length; i++)
+                {
+                    if (siblings[i].ID == currentItem.ID)
+                    {
+                        currentIndex = i;
+                        break;
+                    }
+                }
+
+                if (currentIndex < siblings.Length - 1)
+                {
+                    return siblings[currentIndex + 1];
+                }
+            }
+
+            return null;
+        }
+    }
+}
