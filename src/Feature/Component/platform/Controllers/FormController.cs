1d32d84412ee6f3be6d861d3b6e7a7968edc22af
Cheryl Chang <cheryl.chang@esi-tech.net>
2024-04-18 12:15:40 +0800

    Add: D25.表格元件

---
+++ src/Feature/Component/platform/Controllers/FormController.cs
@@ -0,0 +1,33 @@
+﻿using Feature.Wealth.Component.Models.Form;
+using Sitecore.Mvc.Presentation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class FormController : Controller
+    {
+        public ActionResult Index()
+        {
+            var dataSourceItem = RenderingContext.CurrentOrNull?.Rendering.Item;
+            var childItems = ItemUtils.GetChildren(dataSourceItem).ToList();
+
+            var items = new List<FormModel.Form>();
+
+            foreach (var childItem in childItems)
+            {
+                items.Add(new FormModel.Form(childItem));
+            }
+
+            var model = new FormModel
+            {
+                DataSource = dataSourceItem,
+                Items = items
+            };
+
+            return View("/Views/Feature/Wealth/Component/Form/Form.cshtml", model);
+        }
+    }
+}
