791625448f9b50b0dea8ae9b9f5c4167a2c1d0bb
Andrew Wu <andrew.wu@esi-tech.net>
2024-04-18 12:12:27 +0800

    add

--- src/Feature/Component/platform/Repositories/GlobalIndexRepository.cs
+++ src/Feature/Component/platform/Repositories/GlobalIndexRepository.cs
@@ -1,5 +1,4 @@
 ﻿using Dapper;
-using Feature.Wealth.Component.Models.FundReturn;
 using Feature.Wealth.Component.Models.GlobalIndex;
 using Foundation.Wealth.Manager;
 using System.Collections.Generic;
@@ -22,13 +21,13 @@ namespace Feature.Wealth.Component.Repositories
                            ,REPLACE(CONVERT(char(10), [DataDate],126),'-','/') [DataDate]
                            ,[MarketPrice]
                            ,CONVERT(nvarchar, CONVERT(MONEY, [MarketPrice]), 1) [MarketPriceText]
-                           ,CONVERT(nvarchar, CONVERT(MONEY, [Change]), 1) [Change]
+                           ,CONVERT(nvarchar, CONVERT(MONEY, [Change]), 1)　[Change]
                            ,CONVERT(nvarchar, CONVERT(decimal(16,2), [ChangePercentage])) + '%' [ChangePercentage]
                            ,CONVERT(bit, IIF([Change] >= 0, 1, 0)) [UpOrDown]
                            FROM [Sysjust_GlobalIndex] WITH (NOLOCK)
                            ORDER BY [IndexCode]";
 
-            var globalIndexs = _dbConnection.Query<GlobalIndex>(sql)?.ToList() ?? new List<GlobalIndex>();
+            var globalIndexs = this._dbConnection.Query<GlobalIndex>(sql)?.ToList() ?? new List<GlobalIndex>();
 
             return globalIndexs;
         }
@@ -43,7 +42,7 @@ namespace Feature.Wealth.Component.Repositories
                            ,REPLACE(CONVERT(char(10), [DataDate],126),'-','/') [DataDate]
                            ,[MarketPrice]
                            ,CONVERT(nvarchar, CONVERT(MONEY, [MarketPrice]), 1) [MarketPriceText]
-                           ,CONVERT(nvarchar, CONVERT(MONEY, [Change]), 1) [Change]
+                           ,CONVERT(nvarchar, CONVERT(MONEY, [Change]), 1)　[Change]
                            ,CONVERT(nvarchar, CONVERT(decimal(16,2), [ChangePercentage])) + '%' [ChangePercentage]
                            ,CONVERT(bit, IIF([Change] >= 0, 1, 0)) [UpOrDown]
                            FROM [Sysjust_GlobalIndex] WITH (NOLOCK)
@@ -56,7 +55,7 @@ namespace Feature.Wealth.Component.Repositories
                            )
                            ORDER BY [IndexCode]";
 
-            var globalIndexs = _dbConnection.Query<GlobalIndex>(sql)?.ToList() ?? new List<GlobalIndex>();
+            var globalIndexs = this._dbConnection.Query<GlobalIndex>(sql)?.ToList() ?? new List<GlobalIndex>();
 
             return globalIndexs;
         }
@@ -72,16 +71,74 @@ namespace Feature.Wealth.Component.Repositories
                            ,REPLACE(CONVERT(char(10), [DataDate],126),'-','/') [DataDate]
                            ,[MarketPrice]
                            ,CONVERT(nvarchar, CONVERT(MONEY, [MarketPrice]), 1) [MarketPriceText]
-                           ,CONVERT(nvarchar, CONVERT(MONEY, [Change]), 1) [Change]
+                           ,CONVERT(nvarchar, CONVERT(MONEY, [Change]), 1)　[Change]
                            ,CONVERT(nvarchar, CONVERT(decimal(16,2), [ChangePercentage])) + '%' [ChangePercentage]
                            ,CONVERT(bit, IIF([Change] >= 0, 1, 0)) [UpOrDown]
                            FROM [Sysjust_GlobalIndex_History] WITH (NOLOCK)
                            WHERE IndexCode = @IndexCode
                            ORDER BY [DataDate] DESC";
 
-            var globalIndexs = _dbConnection.Query<GlobalIndex>(sql, new { IndexCode = indexCode })?.ToList() ?? new List<GlobalIndex>();
+            var globalIndexs = this._dbConnection.Query<GlobalIndex>(sql, new { IndexCode = indexCode })?.ToList() ?? new List<GlobalIndex>();
 
             return globalIndexs;
+        }
+
+        public GlobalIndexDetail GetGlobalIndexDetail(string indexCode)
+        {
+            string sql = @"SELECT
+                           A.[IndexCode]
+                           ,[IndexName]
+                           ,[IndexCategoryID]
+                           ,[IndexCategoryName]
+                           ,REPLACE(CONVERT(char(10), [DataDate],126),'-','/') [DataDate]
+                           ,[MarketPrice]
+                           ,CONVERT(nvarchar, CONVERT(MONEY, [MarketPrice]), 1) [MarketPriceText]
+                           ,CONVERT(nvarchar, CONVERT(MONEY, [Change]), 1)　[Change]
+                           ,CONVERT(nvarchar, CONVERT(decimal(16,2), [ChangePercentage])) + '%' [ChangePercentage]
+                           ,CONVERT(bit, IIF([Change] >= 0, 1, 0)) [UpOrDown]
+                           ,B.[ViewCount]
+                           FROM [Sysjust_GlobalIndex] A WITH (NOLOCK)
+                           LEFT JOIN [GlobalIndexViewCount] B WITH (NOLOCK) ON A.[IndexCode] = B.[IndexCode]
+                           WHERE A.[IndexCode] = @IndexCode
+                           ORDER BY [IndexCode]";
+
+            var globalIndexDetail = this._dbConnection.Query<GlobalIndexDetail>(sql, new { IndexCode = indexCode })?.FirstOrDefault() ?? new GlobalIndexDetail();
+
+            return globalIndexDetail;
+        }
+
+        /// <summary>
+        /// 暫定 觸發紀錄指數瀏覽sp
+        /// </summary>
+        /// <param name="indexCode"></param>
+        /// <returns></returns>
+        public bool TriggerViewCountRecord(string indexCode)
+        {
+            int updateCount = 0;
+            var para = new { IndexCode = indexCode };
+            updateCount = DbManager.Custom.Execute<int>("sp_GlobalIndexViewCountRecord", para, commandType: System.Data.CommandType.StoredProcedure);
+            return updateCount == 1;
+        }
+
+        public decimal? Round2(decimal? value)
+        {
+            if (value != null)
+            {
+                value = decimal.Round((decimal)value, 2);
+            }
+            return value;
+        }
+
+        public double GetDoubleOrZero(string value)
+        {
+            if (double.TryParse(value, out double reslut))
+            {
+                return reslut;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
