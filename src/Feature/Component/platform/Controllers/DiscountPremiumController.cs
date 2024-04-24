f6d95d02443249a7c952c4a1371c78ad82e551f0
Esther Lin <esther.lin@esi-tech.net>
2024-04-24 19:04:40 +0800

    Add: C16-Discount-Premium - 折溢價排行

---
+++ src/Feature/Component/platform/Controllers/DiscountPremiumController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Sitecore.Mvc.Presentation;
+using System.Collections.Generic;
+using Feature.Wealth.Component.Repositories;
+using Xcms.Sitecore.Foundation.Basic.Extensions;
+using Feature.Wealth.Component.Models.FundDetail;
+using Feature.Wealth.Component.Models.DiscountPremium;
+using static Feature.Wealth.Component.Models.DiscountPremium.DiscountPremiumModel;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class DiscountPremiumController : Controller
+    {
+        private DiscountPremiumRepository _repository = new DiscountPremiumRepository();
+
+        public ActionResult Index()
+        {
+            var dataSourceItem = RenderingContext.CurrentOrNull.Rendering.Item;
+
+            var etfs = _repository.GetFundData();
+            var viewModel = new DiscountPremiumModel
+            {
+                Item = dataSourceItem,
+                DiscountPremiums = etfs,
+                DetailLink = FundRelatedSettingModel.GetFundDetailsUrl()
+            };
+
+            return View("/Views/Feature/Wealth/Component/DiscountPremium/DiscountPremium.cshtml", viewModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult GetSortedDiscountPremium(string orderby, string desc)
+        {
+            if (orderby.IsNullOrEmpty()) { orderby = "DiscountPremium"; }
+            if (desc.IsNullOrEmpty()) { desc = "is-desc"; }
+
+            var etfs = _repository.GetFundData();
+
+            var property = typeof(ETFs).GetProperty(orderby);
+            if (desc.Equals("is-desc", StringComparison.OrdinalIgnoreCase))
+            {
+                etfs = etfs.OrderByDescending(f => property.GetValue(f, null)).ToList();
+            }
+            else
+            {
+                etfs = etfs.OrderBy(f => property.GetValue(f, null)).ToList();
+            }
+
+            var viewModel = new DiscountPremiumModel
+            {
+                DiscountPremiums = etfs
+            };
+
+            return View("/Views/Feature/Wealth/Component/DiscountPremium/DiscountPremiumReturnView.cshtml", viewModel);
+        }
+    }
+}
---
+++ src/Feature/Component/platform/Repositories/DiscountPremiumRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using System.Text;
+using Foundation.Wealth.Manager;
+using System.Collections.Generic;
+using static Feature.Wealth.Component.Models.DiscountPremium.DiscountPremiumModel;
+
+namespace Feature.Wealth.Component.Repositories
+{
+    public class DiscountPremiumRepository
+    {
+        public IList<ETFs> GetFundData()
+        {
+            List<ETFs> ETFItems = new List<ETFs>();
+            string sql = """
+                   SELECT Top 10 *
+                   FROM [vw_BasicETF] 
+                   ORDER BY DiscountPremium
+                   DESC,ProductCode
+                   """;
+
+            var results = DbManager.Custom.ExecuteIList<ETFs>(sql, null, CommandType.Text);
+            foreach (var item in results)
+            {
+                ProcessFundFilterDatas(item);
+                ETFItems.Add(item);
+            }
+            return ETFItems;
+
+        }
+        private void ProcessFundFilterDatas(ETFs item)
+        {
+            item.ProductName = item.ProductName.Normalize(NormalizationForm.FormKC).TrimEnd(' ');
+            item.DiscountPremium = decimal.Round(item.DiscountPremium, 4);
+            item.MarketPrice = decimal.Round(item.MarketPrice, 4);
+        }
+    }
+}
