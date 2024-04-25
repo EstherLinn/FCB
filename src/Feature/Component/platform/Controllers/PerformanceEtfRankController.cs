feac0b9c23ba2ea722492f89a2aa056a8c33ee80
Esther Lin <esther.lin@esi-tech.net>
2024-04-25 19:20:49 +0800

    Add: C14-Performance-Rank - 績效排行

---
+++ src/Feature/Component/platform/Controllers/PerformanceEtfRankController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Sitecore.Mvc.Presentation;
+using System.Collections.Generic;
+using Feature.Wealth.Component.Repositories;
+using Xcms.Sitecore.Foundation.Basic.Extensions;
+using Feature.Wealth.Component.Models.FundDetail;
+using Feature.Wealth.Component.Models.PerformanceEtfRank;
+using static Feature.Wealth.Component.Models.PerformanceEtfRank.PerformanceEtfRankModel;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class PerformanceEtfRankController : Controller
+    {
+        private PerformanceEtfRankRepository _repository = new PerformanceEtfRankRepository();
+
+        public ActionResult Index()
+        {
+            var dataSourceItem = RenderingContext.CurrentOrNull.Rendering.Item;
+
+            var etfs = _repository.GetFundData();
+            var viewModel = new PerformanceEtfRankModel
+            {
+                Item = dataSourceItem,
+                PerformanceEtfRanks = etfs,
+                DetailLink = FundRelatedSettingModel.GetFundDetailsUrl()
+            };
+
+            return View("/Views/Feature/Wealth/Component/PerformanceEtfRank/PerformanceEtfRank.cshtml", viewModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult GetSortedPerformanceEtfRank(string orderby, string desc)
+        {
+            if (orderby.IsNullOrEmpty()) { orderby = "DiscountPremium"; }
+            if (desc.IsNullOrEmpty()) { desc = "is-desc"; }
+
+            var etfs = _repository.GetFundData();
+
+            var property = typeof(ETFs).GetProperty(orderby);
+            if (desc.Equals("is-desc", StringComparison.OrdinalIgnoreCase))
+            {
+                etfs = etfs.OrderByDescending(f => property.GetValue(f, null)).ToList();
+            }
+            else
+            {
+                etfs = etfs.OrderBy(f => property.GetValue(f, null)).ToList();
+            }
+
+            var viewModel = new PerformanceEtfRankModel
+            {
+                PerformanceEtfRanks = etfs
+            };
+
+            return View("/Views/Feature/Wealth/Component/PerformanceEtfRank/PerformanceEtfRankReturnView.cshtml", viewModel);
+        }
+    }
+}
