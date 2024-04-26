ba7c7bdf4ab7129738ae53709b13846109fb23d9
Esther Lin <esther.lin@esi-tech.net>
2024-04-26 16:49:39 +0800

    Add: C17-New-Arrival -近期上架ETF

---
+++ src/Feature/Component/platform/Repositories/NewArrivalEtfRepository.cs
@@ -0,0 +1,68 @@
+﻿using Foundation.Wealth.Manager;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Feature.Wealth.Component.Models.NewArrivalETF.NewArrivalEtfModel;
+
+namespace Feature.Wealth.Component.Repositories
+{
+    public class NewArrivalEtfRepository
+    {
+        public List<ETFs> GetFundData()
+        {
+            List<ETFs> fundItems = new List<ETFs>();
+
+            var sql = """
+             SELECT *
+             FROM [vw_BasicETF]
+             WHERE [ListingDate] >= DATEADD(year, -1, GETDATE())
+             ORDER BY SixMonthReturnMarketPriceOriginalCurrency
+             DESC
+             """;
+
+            var results = DbManager.Custom.ExecuteIList<ETFs>(sql, null, CommandType.Text);
+
+            foreach (var item in results)
+            {
+                ProcessFundFilterDatas(item);
+            }
+
+            fundItems.AddRange(results);
+
+            return fundItems;
+        }
+
+        private void ProcessFundFilterDatas(ETFs item)
+        {
+            item.ProductName = item.ProductName.Normalize(NormalizationForm.FormKC);
+            item.DiscountPremiumFormat = RoundingPrice(item.DiscountPremium);
+            item.SixMonthReturnMarketPriceOriginalCurrencyFormat = RoundingPrice2(item.SixMonthReturnMarketPriceOriginalCurrency);
+            item.MarketPriceFormat = RoundingPrice(item.MarketPrice);
+        }
+
+        private decimal? RoundingPrice(decimal? number)
+        {
+            if (!number.HasValue)
+            {
+                return null;
+            }
+
+            return Math.Round(number.Value, 4, MidpointRounding.AwayFromZero);
+        }
+
+        private decimal? RoundingPrice2(decimal? number)
+        {
+            if (!number.HasValue)
+            {
+                return null;
+            }
+
+            return Math.Round(number.Value, 2, MidpointRounding.AwayFromZero);
+        }
+
+
+    }
+}
