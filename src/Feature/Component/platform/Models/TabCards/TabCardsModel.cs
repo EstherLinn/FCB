57b041d3e4525f40a6e77decb8033aa05e4943b6
Esther Lin <esther.lin@esi-tech.net>
2024-04-29 11:41:41 +0800

    Update: 判斷有無值更改

--- src/Feature/Component/platform/Models/TabCards/TabCardsModel.cs
+++ src/Feature/Component/platform/Models/TabCards/TabCardsModel.cs
@@ -11,18 +11,24 @@ namespace Feature.Wealth.Component.Models.TabCards
     public class TabCardsModel
     {
         public Item Datasource { get; private set; }
-
+        public string Title1 => ItemUtils.GetFieldValue(Datasource,_TabCard.Fields.Title1);
         public string ImageUrl1 { get; set; }
         public string BannerLink1 { get; set; }
         public bool IsBlackFont1 => ItemUtils.IsChecked(Datasource, _TabCard.Fields.IsBlackFont1);
+        public string Description1 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Description1);
 
+
+        public string Title2 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Title2);
         public string ImageUrl2 { get; set; }
         public string BannerLink2 { get; set; }
         public bool IsBlackFont2 => ItemUtils.IsChecked(Datasource, _TabCard.Fields.IsBlackFont2);
+        public string Description2 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Description2);
 
+        public string Title3 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Title3);
         public string ImageUrl3 { get; set; }
         public string BannerLink3 { get; set; }
         public bool IsBlackFont3 => ItemUtils.IsChecked(Datasource, _TabCard.Fields.IsBlackFont3);
+        public string Description3 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Description3);
 
         public IList<string> FundIDList { get; set; }
         public IList<FundCardBasicDTO> FundCardsInfos { get; set; }
