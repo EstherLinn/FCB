49e045c0f79296b5f504eb80a87028c1edf4ae53
Esther Lin <esther.lin@esi-tech.net>
2024-05-01 17:51:36 +0800

    Add: B06首頁熱門推薦元件

---
+++ src/Feature/Component/platform/Controllers/IndexRecommendationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using Sitecore.Mvc.Presentation;
+using Feature.Wealth.Component.Repositories;
+using Feature.Wealth.Component.Models.FundDetail;
+using Feature.Wealth.Component.Models.IndexRecommendation;
+using System.Linq;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class IndexRecommendationController : Controller
+    {
+        private IndexRecommendationRepository _repository = new IndexRecommendationRepository();
+
+        public ActionResult Index()
+        {
+            var dataSourceItem = RenderingContext.CurrentOrNull.Rendering.Item;
+            var funds = _repository.GetFundData();
+            var viewModel = new IndexRecommendationModel
+            {
+                Item = dataSourceItem,
+                HotFunds=funds,
+                FundDetailLink = FundRelatedSettingModel.GetFundDetailsUrl(),
+                NetAssetValueDate = funds.Select(f => f.NetAssetValueDate).FirstOrDefault()
+            };
+
+            return View("/Views/Feature/Wealth/Component/IndexRecommendation/IndexRecommendation.cshtml", viewModel);
+        }
+    }
+}
---
+++ src/Feature/Component/platform/Repositories/IndexRecommendationRepository.cs
@@ -0,0 +1,62 @@
+﻿using System.Data;
+using Foundation.Wealth.Manager;
+using System.Collections.Generic;
+using Foundation.Wealth.Extensions;
+using Feature.Wealth.Component.Models.IndexRecommendation;
+
+namespace Feature.Wealth.Component.Repositories
+{
+    public class IndexRecommendationRepository
+    {
+        public IList<Funds> GetFundData()
+        {
+            List<Funds> fundItems = new List<Funds>();
+
+            string sql = """
+                   SELECT Top 5 *
+                   FROM [vw_BasicFund] 
+                   ORDER BY ViewCount
+                   DESC,ProductCode
+                   """;
+
+            var results = DbManager.Custom.ExecuteIList<Funds>(sql, null, CommandType.Text);
+
+            foreach (var item in results)
+            {
+                ProcessFundFilterDatas(item);
+                fundItems.Add(item);
+            }
+            return fundItems;
+
+        }
+
+        private void ProcessFundFilterDatas(Funds item)
+        {
+            item.SixMonthReturnOriginalCurrency = NumberExtensions.RoundingPercentage(item.SixMonthReturnOriginalCurrency);
+            item.NetAssetValue = NumberExtensions.RoundingValue(item.NetAssetValue);
+            item.PercentageChangeInFundPrice = NumberExtensions.RoundingPercentage((item.PercentageChangeInFundPrice * 100));
+            if (item.SixMonthReturnOriginalCurrency < 0)
+            {
+                item.SixMonthReturnOriginalCurrencyFormat = item.SixMonthReturnOriginalCurrency.ToString().Substring(1);
+            }
+            if (item.PercentageChangeInFundPrice < 0)
+            {
+                item.PercentageChangeInFundPriceFormat = item.PercentageChangeInFundPrice.ToString().Substring(1);
+            }
+        }
+
+        public IList<ETFs> GetETFData()
+        {
+            string sql = """
+                   SELECT *
+                   FROM [vw_BasicETF] 
+                   ORDER BY ViewCount
+                   DESC,ProductCode
+                   """;
+
+            var results = DbManager.Custom.ExecuteIList<ETFs>(sql, null, CommandType.Text);
+            return results;
+
+        }
+    }
+}
