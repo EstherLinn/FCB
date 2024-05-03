4fd24e3bbee1ac21836cdf6dc76640915a8734d2
Cheryl Chang <cheryl.chang@esi-tech.net>
2024-05-03 18:29:01 +0800

    Update: D10.公告事項與警語元件

--- src/Feature/Component/platform/Controllers/AnnouncementsAndWarningsController.cs
+++ src/Feature/Component/platform/Controllers/AnnouncementsAndWarningsController.cs
@@ -13,50 +13,14 @@ namespace Feature.Wealth.Component.Controllers
     {
         public ActionResult Index()
         {
-            var datasource = RenderingContext.CurrentOrNull?.Rendering.Item;
-
-            return View("/Views/Feature/Wealth/Component/AnnouncementsAndWarnings/AnnouncementsAndWarnings.cshtml", CreateModel(datasource));
-        }
-
-        protected TabModel CreateModel(Item item)
-        {
-            var model = new TabModel(item);
-            return model;
-        }
-
-        public ActionResult Announcements()
-        {
-            var model = new AnnouncementsAndWarningsModel()
-            {
-                Item = RenderingContext.CurrentOrNull?.Rendering.Item
-            };
-
-            return View("/Views/Feature/Wealth/Component/AnnouncementsAndWarnings/Announcements.cshtml", model);
-        }
-
-        public ActionResult MoreAnnouncements()
-        {
             var item = RenderingContext.CurrentOrNull?.Rendering.Item;
 
-            var imageUrl = ItemUtils.ImageUrl(item, Templates.MoreAnnouncement.Fields.Image);
+            var imageUrl = ItemUtils.ImageUrl(item, Templates.AnnouncementsandWarnings.Fields.Image);
 
-            var model = new AnnouncementsAndWarningsModel()
-            {
-                Item = item,
-                ImageUrl = imageUrl,
-            };
+            var model = new AnnouncementsAndWarningsModel(item);
 
-            return View("/Views/Feature/Wealth/Component/AnnouncementsAndWarnings/MoreAnnouncements.cshtml", model);
+            return View("/Views/Feature/Wealth/Component/AnnouncementsAndWarnings/AnnouncementsAndWarnings.cshtml", model);
         }
 
-        public ActionResult Warnings()
-        {
-            var model = new NoticeModel()
-            {
-                Item = RenderingContext.CurrentOrNull?.Rendering.Item
-            };
-
-            return View("/Views/Feature/Wealth/Component/AnnouncementsAndWarnings/Warnings.cshtml", model);
-        }
     }
 }
