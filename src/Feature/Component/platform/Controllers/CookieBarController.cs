2b456c0d89fe18a8800b163990ca0750a5ef29c7
Michael Lee <michael.lee@esi-tech.net>
2024-05-06 19:07:48 +0800

    Add : 新增 A05_CookieBar 元件

---
+++ src/Feature/Component/platform/Controllers/CookieBarController.cs
@@ -0,0 +1,56 @@
+﻿using Feature.Wealth.Component.Models.CookieBar;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Web;
+using System.Web.Mvc;
+using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class CookieBarController : Controller
+    {
+        public ActionResult Index()
+        {
+            var dataSource = RenderingContext.CurrentOrNull?.Rendering.Item;
+            var content = ItemUtils.GetFieldValue(dataSource, Templates.CookieBarDatasource.Fields.Content);
+            var buttonText = ItemUtils.GetFieldValue(dataSource, Templates.CookieBarDatasource.Fields.ButtonText);
+            var style = System.Web.HttpContext.Current.Request.Cookies["accept_Cookie"] != null ? "display:none;" : string.Empty;
+
+            var model = new CookieBarModel()
+            {
+                DataSource = dataSource,
+                Content = content,
+                ButtonText = buttonText,
+                Style = style
+            };
+
+            return View("/Views/Feature/Wealth/Component/CookieBar/CookieBar.cshtml", model);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCookie()
+        {
+            try
+            {
+                if (Request.Cookies["accept_Cookie"] == null)
+                {
+                    var cookie = new HttpCookie("accept_Cookie")
+                    {
+                        Value = "1",
+                        Expires = DateTime.Now.AddDays(1),
+                        Path = "/",
+                        HttpOnly = true
+                    };
+
+                    Response.Cookies.Add(cookie);
+                }
+
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+    }
+}
---
+++ src/Feature/Component/platform/Models/CookieBar/CookieBarModel.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+
+namespace Feature.Wealth.Component.Models.CookieBar
+{
+    public class CookieBarModel
+    {
+        public Item DataSource { get; set; }
+        public string Content { get; set; }
+        public string ButtonText { get; set; }
+        public string Style { get; set; }
+    }
+
+    public struct Templates
+    {
+        public struct CookieBarDatasource
+        {
+            public static readonly ID Id = new ID("{111DC9CC-9DD3-4C81-B8FD-3D60EFCD8621}");
+
+            public struct Fields
+            {
+                public static readonly ID Content = new ID("{6646B588-72B3-40E3-89F3-EA6C3A740389}");
+                public static readonly ID ButtonText = new ID("{DEA01F95-D474-4254-8C56-D65D9354AECF}");
+            }
+        }
+    }
+}
