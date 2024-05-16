0c1a074baa8fad1d276b1313fe38ebd396f696b0
Esther Lin <esther.lin@esi-tech.net>
2024-05-16 11:20:31 +0800

    Update: 手機板電腦版申購按鈕樣式

--- src/Feature/Component/platform/Repositories/NewFundRepository.cs
+++ src/Feature/Component/platform/Repositories/NewFundRepository.cs
@@ -25,7 +25,11 @@ namespace Feature.Wealth.Component.Repositories
 
             foreach (var item in results)
             {
-                ProcessFundFilterDatas(item);
+                if (item != null) 
+                {
+                    ProcessFundFilterDatas(item);
+                    fundItems.Add(item);
+                }
             }
 
             fundItems.AddRange(results);
