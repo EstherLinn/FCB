b294d55d7bc5fd0762861f56c27b87789f45e748
Esther Lin <esther.lin@esi-tech.net>
2024-05-22 10:52:48 +0800

    Add: ETF熱門推薦排行榜

--- src/Feature/Component/platform/Repositories/IndexRecommendationRepository.cs
+++ src/Feature/Component/platform/Repositories/IndexRecommendationRepository.cs
@@ -1,14 +1,19 @@
-﻿using System.Data;
+﻿using System.Text;
+using System.Linq;
+using System.Data;
 using Foundation.Wealth.Manager;
 using System.Collections.Generic;
 using Foundation.Wealth.Extensions;
+using Feature.Wealth.Component.Models.ETF;
+using Feature.Wealth.Component.Models.ETF.Tag;
 using Feature.Wealth.Component.Models.IndexRecommendation;
-using System.Text;
 
 namespace Feature.Wealth.Component.Repositories
 {
     public class IndexRecommendationRepository
     {
+        private readonly VisitCountRepository _repository = new VisitCountRepository();
+
         public IList<Funds> GetFundData()
         {
             List<Funds> fundItems = new List<Funds>();
@@ -49,16 +54,69 @@ namespace Feature.Wealth.Component.Repositories
 
         public IList<ETFs> GetETFData()
         {
+            List<ETFs> etfsItems = new List<ETFs>();
             string sql = """
                    SELECT *
-                   FROM [vw_BasicETF] 
-                   ORDER BY ViewCount
+                   FROM [vw_BasicETF]
+                   ORDER BY SixMonthReturnMarketPriceOriginalCurrency
                    DESC,ProductCode
                    """;
 
             var results = DbManager.Custom.ExecuteIList<ETFs>(sql, null, CommandType.Text);
-            return results;
 
+            foreach (var item in results)
+            {
+                item.ProductName = item.ProductName.Normalize(NormalizationForm.FormKC);
+                etfsItems.Add(item);
+            }
+            return etfsItems;
         }
+
+        public IList<ETFs> GetETFDatas()
+        {
+            var queryitem = EtfRelatedLinkSetting.GetETFDetailPageItem();
+            var query = queryitem.ID.ToGuid();
+            var etfData = GetETFData();
+
+            var etfs = _repository.GetVisitRecords(query, "id");
+
+            if (etfs == null || !etfs.Any())
+            {
+                return new List<ETFs>();
+            }
+
+            var etfIds = etfs
+                        .OrderByDescending(x => x.VisitCount)
+                        .Take(5)
+                        .SelectMany(x => x.QueryStrings)
+                        .Where(x => x.Key.Equals("id"))
+                        .Select(x => x.Value)
+                        .ToList();
+
+
+            var results = etfData
+            .Where(e => etfIds.Contains(e.ProductCode))
+            .OrderBy(e => etfIds.IndexOf(e.ProductCode.ToString()))
+            .ToList();
+
+            EtfTagRepository tagRepository = new EtfTagRepository();
+            var dicTag = tagRepository.GetTagCollection();
+
+            foreach (var item in results)
+            {
+                if (dicTag.ContainsKey(TagType.Discount))
+                {
+                    var discountTags = dicTag[TagType.Discount]
+                        .Where(tag => tag.ProductCodes.Contains(item.ProductCode.ToString()))
+                        .Select(tag => tag.TagKey)
+                        .ToArray();
+
+                    item.ETFDiscountTags = discountTags;
+                }
+            }
+
+            return results;
+        }
+
     }
 }
