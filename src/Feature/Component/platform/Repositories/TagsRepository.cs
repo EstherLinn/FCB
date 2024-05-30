067913160f6c2da96d7877c307390e760b5f0f0b
Esther Lin <esther.lin@esi-tech.net>
2024-05-30 15:50:43 +0800

    基金搜尋申購、貼標問題

--- src/Feature/Component/platform/Repositories/TagsRepository.cs
+++ src/Feature/Component/platform/Repositories/TagsRepository.cs
@@ -3,29 +3,35 @@ using System.Linq;
 using Sitecore.Data.Items;
 using System.Collections.Generic;
 using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
-using static Feature.Wealth.Component.Models.Tags.TagsModel;
+using static Feature.Wealth.Component.Models.Tags.FundTagsModel;
 
 namespace Feature.Wealth.Component.Repositories
 {
     public class TagsRepository
     {
-        public List<Tags> GetTagData()
+        public List<Tags> GetFundTagData()
         {
             List<Tags> TagModels = new List<Tags>();
 
             Item TagsFolder = ItemUtils.GetItem(Template.Fields.TagsFolder);
 
-            foreach (var item in TagsFolder.GetDescendants(Template.Fields.FundTags))
+            foreach (var item in TagsFolder.GetChildren(Template.Fields.TagFolder))
             {
-                TagModels.Add(new Tags()
+                foreach (var i in item.GetChildren(Template.Fields.FundTags))
                 {
-                    TagName = item[Template.Fields.TagName],
-                    ProductCodes = item[Template.Fields.ProductCodeList].Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList(),
-                    FundType = item[Template.Fields.FundType]
-                });
+                    Tags tagModel = new Tags()
+                    {
+                        FundType = item[Template.Fields.FundType],
+                        TagName = i[Template.Fields.TagName],
+                        ProductCodes = i[Template.Fields.ProductCodeList].Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList()
+                    };
+
+                    TagModels.Add(tagModel);
+                }
             }
-            
+
             return TagModels;
         }
+
     }
 }
