6eb50edee6f6397d1b0cb9eda2c6d10dbcd0d253
Mice.Lin <mice.lin@esi-tech.net>
2024-06-03 01:09:01 +0800

    A09.社群工具(social widget) 含加入常用

--- src/Feature/Account/platform/Models/Api/CommonFuncrionsResp.cs
+++ src/Feature/Account/platform/Models/Api/CommonFuncrionsResp.cs
@@ -1,19 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using Sitecore.Services.Infrastructure.Security;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 
 namespace Feature.Wealth.Account.Models.Api
 {
-   public class CommonFuncrionsResp: IRespModel<IEnumerable<CommonFunctionsModel>>
+    public class CommonFuncrionsResp : IRespModel<IEnumerable<CommonFunctionsModel>>
     {
         public HttpStatusCode StatusCode { get; set; }
         public string Message { get; set; }
         public IEnumerable<CommonFunctionsModel> Body { get; set; }
 
-        public CommonFuncrionsResp() {
+        public CommonFuncrionsResp()
+        {
             StatusCode = HttpStatusCode.OK;
             Message = string.Empty;
             Body = Enumerable.Empty<CommonFunctionsModel>().ToList();
+        }
+    }
+
+    public class CommonToolsRespResp : CommonFuncrionsResp
+    {
+        public IEnumerable<CommonFunctionsModel> Tools { get; set; }
+
+        public CommonToolsRespResp() : base()
+        {
+           var tools = Enumerable.Empty<CommonFunctionsModel>().ToList();
+            tools.Add(new CommonFunctionsModel
+            {
+                PageGuid = "{1D04400C-B8CF-4D54-BC3B-F642D77E1296}"
+            });
+            this.Tools = tools;
         }
     }
 }
