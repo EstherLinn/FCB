cb7c31d7cfe4af871510166de40e7e3347cc4165
Jenny Yeh <jenny.yeh@esi-tech.net>
2024-06-05 20:36:47 +0800

    ETF 詳細頁埋 Log

--- src/Feature/Component/platform/Models/ETF/Detail/EtfKLineChart.cs
+++ src/Feature/Component/platform/Models/ETF/Detail/EtfKLineChart.cs
@@ -4,15 +4,27 @@ namespace Feature.Wealth.Component.Models.ETF.Detail
 {
     public enum EtfKLineChart
     {
+        /// <summary>
+        /// 日
+        /// </summary>
         [Description("D")]
         day,
 
+        /// <summary>
+        /// 週
+        /// </summary>
         [Description("W")]
         week,
 
+        /// <summary>
+        /// 月
+        /// </summary>
         [Description("M")]
         month,
 
+        /// <summary>
+        /// 季
+        /// </summary>
         [Description("Q")]
         season,
     }
---
+++ src/Feature/Component/platform/Models/ETF/RespEtf.cs
@@ -0,0 +1,11 @@
+﻿namespace Feature.Wealth.Component.Models.ETF
+{
+    public class RespEtf
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public dynamic Body { get; set; }
+    }
+}
