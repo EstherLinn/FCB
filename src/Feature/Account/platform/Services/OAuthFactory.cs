266f2c250f211de2de537d22c7539809867a36c5
Jason Hsieh <jason.hsieh@esi-tech.net>
2024-06-18 17:05:07 +0800

    update: 加入壓測 測試登入元件，returnurl加入log

--- src/Feature/Account/platform/Services/OAuthFactory.cs
+++ src/Feature/Account/platform/Services/OAuthFactory.cs
@@ -2,6 +2,7 @@
 using Sitecore.Configuration;
 using System;
 using System.Web;
+using Xcms.Sitecore.Foundation.Basic.Logging;
 
 namespace Feature.Wealth.Account.Services
 {
@@ -10,7 +11,6 @@ namespace Feature.Wealth.Account.Services
         public static string GetAuthUrl(PlatFormEunm oAuthEunm)
         {
             var domain = string.IsNullOrEmpty(Sitecore.Context.Site.TargetHostName) ? HttpContext.Current.Request.Url.Host : Sitecore.Context.Site.TargetHostName;
-
             Guid state = Guid.NewGuid();
             switch (oAuthEunm)
             {
@@ -18,12 +18,13 @@ namespace Feature.Wealth.Account.Services
                     string fbAppId = Settings.GetSetting("Facebook.AppId");
                     string fbAuthUrl = Settings.GetSetting("Facebook.AuthUrl");
                     string fbRedirectUrl = $"https://{domain}/api/client/Accounts/SignInFacebook";
+                    Logger.Account.Info($"{oAuthEunm.ToString()} RedirectUrl: {fbRedirectUrl},TargetHostName={Sitecore.Context.Site.TargetHostName},Request.Url.Host ={HttpContext.Current.Request.Url.Host}");
                     return fbAuthUrl.Replace("{AppId}", fbAppId).Replace("{RedirectUrl}", fbRedirectUrl).Replace("{state}", state.ToString());
                 case PlatFormEunm.Line:
                     string lineAppId = Settings.GetSetting("Line.ClientId");
                     string lineAuthUrl = Settings.GetSetting("Line.AuthUrl");
                     string lineRedirectUrl = $"https://{domain}/api/client/Accounts/SignInLine";
-
+                    Logger.Account.Info($"{oAuthEunm.ToString()} RedirectUrl: {lineRedirectUrl},TargetHostName={Sitecore.Context.Site.TargetHostName},Request.Url.Host ={HttpContext.Current.Request.Url.Host}");
                     return lineAuthUrl.Replace("{scope}", HttpUtility.UrlEncode("profile email openid")).Replace("{state}", state.ToString()).Replace("{RedirectUrl}", lineRedirectUrl).Replace("{clientid}", lineAppId);
                 default:
                     return string.Empty;
