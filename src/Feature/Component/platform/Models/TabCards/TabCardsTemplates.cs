8a9f007b40eec7b7136815e98c505f915cb1465f
shuttle.hong <shuttle.hong@esi-tech.net>
2024-06-18 15:17:13 +0800

    Update:B03首頁Tab大牌卡元件修改成8張

--- src/Feature/Component/platform/Models/TabCards/TabCardsTemplates.cs
+++ src/Feature/Component/platform/Models/TabCards/TabCardsTemplates.cs
@@ -52,6 +52,31 @@ namespace Feature.Wealth.Component.Models.TabCards
             public static readonly ID Title3 = new ID("{3373B166-4B4E-474F-A759-97B9B1FA5128}");
             public static readonly ID Description3 = new ID("{153DDF46-DD61-410E-80F9-3C73129E4127}");
             public static readonly ID IsBlackFont3 = new ID("{F213149F-9B8D-46BC-BA43-27015AD58733}");
+            public static readonly ID Banner4 = new ID("{54957FE5-628B-44BF-B176-5E666EA5A2A8}");
+            public static readonly ID BannerLink4 = new ID("{1DD8EA4D-8FD7-4CEE-AB9B-A1D7CB641ABE}");
+            public static readonly ID Title4 = new ID("{4A6C4ACA-2727-4735-91F1-61C431F45717}");
+            public static readonly ID Description4 = new ID("{BED28A0C-B67A-41B3-88AF-0B420C5ABC0E}");
+            public static readonly ID IsBlackFont4 = new ID("{07182A75-6549-4E5E-A067-0DC18C7E7E28}");
+            public static readonly ID Banner5 = new ID("{093042B8-336F-414E-901D-7A482567406E}");
+            public static readonly ID BannerLink5 = new ID("{5EA3B7A7-9290-4265-91AC-B812FF219593}");
+            public static readonly ID Title5 = new ID("{E94FED94-6C48-4660-BA11-28FAB93956A4}");
+            public static readonly ID Description5 = new ID("{F650EF0E-8340-4753-8D15-7E4D8784AD09}");
+            public static readonly ID IsBlackFont5 = new ID("{8AD213FB-6AAD-4AF3-89B2-6CF1AAB39354}");
+            public static readonly ID Banner6 = new ID("{360ED0C2-ED6E-46FC-9A2B-E0689767BF58}");
+            public static readonly ID BannerLink6 = new ID("{3AE1F2CC-8667-4211-A868-5DB4746588CB}");
+            public static readonly ID Title6 = new ID("{56208055-0A96-4679-9376-C968F5AD7505}");
+            public static readonly ID Description6 = new ID("{D7DF6822-CB08-4507-9C88-A5F98ECE2250}");
+            public static readonly ID IsBlackFont6 = new ID("{9906D85F-2EE6-4481-9C33-2EE94FF9D971}");
+            public static readonly ID Banner7 = new ID("{BBB035E3-FBE5-45E2-AC1F-7943075D5D87}");
+            public static readonly ID BannerLink7 = new ID("{3E69378C-D049-4565-85DE-83E3F7E91DEB}");
+            public static readonly ID Title7 = new ID("{BCD51DC6-886F-4CDA-BC46-340BAF67F3C0}");
+            public static readonly ID Description7 = new ID("{0ED48DEF-793A-4969-A793-EB5A697C6DEE}");
+            public static readonly ID IsBlackFont7 = new ID("{635B4C8B-12E0-4630-8DCD-CBA36B942F7A}");
+            public static readonly ID Banner8 = new ID("{CB3751AE-B5A1-43CB-BF5A-067014FFB0B6}");
+            public static readonly ID BannerLink8 = new ID("{04C53B63-FD0C-4330-8876-AD77D980FEA4}");
+            public static readonly ID Title8 = new ID("{C1EB3A1F-925B-4D1C-B486-0D197D17854A}");
+            public static readonly ID Description8 = new ID("{56E8C752-E42E-4052-827B-2D421507382C}");
+            public static readonly ID IsBlackFont8 = new ID("{106E1281-2A93-4577-8CB1-6872BEFAB405}");
             public static readonly ID FundIDList = new ID("{420891A7-1186-4135-B33F-14DEEB15FCD1}");
         }
     }
