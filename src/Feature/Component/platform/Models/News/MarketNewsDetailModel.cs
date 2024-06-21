2f651cb642686abb5f2e246a533ea89bfccd3346
Michael Lee <michael.lee@esi-tech.net>
2024-06-21 20:35:25 +0800

    Update : 調整 MarketNewsModel 和 MarketNewsDetailData，把Table屬性抽出來

--- src/Feature/Component/platform/Models/News/MarketNewsDetailModel.cs
+++ src/Feature/Component/platform/Models/News/MarketNewsDetailModel.cs
@@ -2,20 +2,24 @@
 
 namespace Feature.Wealth.Component.Models.News
 {
-    public class MarketNewsDetailModel
-    {
-        public MarketNewsDetailData MarketNewsDetailData { get; set; }
-    }
-
-    public class MarketNewsDetailData
+    public class MarketNewsDetailTableModel
     {
         public string NewsSerialNumber { get; set; }
         public string NewsDetailDate { get; set; }
         public string NewsTitle { get; set; }
         public string NewsContent { get; set; }
-        public HtmlString NewsContentHtmlString { get; set; }
         public string NewsRelatedProducts { get; set; }
         public string NewsType { get; set; }
+    }
+
+    public class MarketNewsDetailModel
+    {
+        public MarketNewsDetailData MarketNewsDetailData { get; set; }
+    }
+
+    public class MarketNewsDetailData : MarketNewsDetailTableModel
+    {
+        public HtmlString NewsContentHtmlString { get; set; }
         public string NewsViewCount { get; set; }
         public string PreviousPageTitle { get; set; }
         public string PreviousPageLink { get; set; }
