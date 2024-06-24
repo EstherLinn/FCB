84d88b53aaffa0dea3c5a1019fc47f99dc8415be
Jason Hsieh <jason.hsieh@esi-tech.net>
2024-06-24 17:25:00 +0800

    update: 調整ileo關注清單同步

---
+++ src/Feature/Account/platform/Models/Api/FocusListResp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Feature.Wealth.Account.Models.Api
+{
+    public class FocusListResp
+    {
+        public string promotionCode { get; set; }
+        public List<TrackModel> TrackList { get; set; }
+        public string rt { get; set; }      
+    }
+    public class TrackModel
+    {
+        public string fundCode { get; set; }
+        public string fundType { get; set; }
+
+    }
+}
