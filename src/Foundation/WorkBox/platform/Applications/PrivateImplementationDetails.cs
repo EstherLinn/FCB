70d4c03764ba888e0bd8b432e05822be4ec31699
Mice.Lin <mice.lin@esi-tech.net>
2024-06-29 01:29:10 +0800

    workflower

---
+++ src/Foundation/WorkBox/platform/Applications/PrivateImplementationDetails.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Foundation.WorkBox.Applications.Workflows
+{
+    // Token: 0x020001EF RID: 495
+    [CompilerGenerated]
+    internal sealed class PrivateImplementationDetails
+    {
+        // Token: 0x06001216 RID: 4630 RVA: 0x00076BAC File Offset: 0x00074DAC
+        internal static uint ComputeStringHash(string s)
+        {
+            uint num = 1;
+            if (s != null)
+            {
+                num = 2166136261U;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    num = ((uint)s[i] ^ num) * 16777619U;
+                }
+            }
+            return num;
+        }
+    }
+}
