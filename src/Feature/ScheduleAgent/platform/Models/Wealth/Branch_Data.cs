c0e4c34cd2983addd740556ab93a60f305326f52
Esther Lin <esther.lin@esi-tech.net>
2024-07-01 17:49:42 +0800

    Update: 分行資料固定位置分割

--- src/Feature/ScheduleAgent/platform/Models/Wealth/Branch_Data.cs
+++ src/Feature/ScheduleAgent/platform/Models/Wealth/Branch_Data.cs
@@ -12,34 +12,34 @@ namespace Feature.Wealth.ScheduleAgent.Models.Wealth
         [FixedWidthLineField(Start = 1, Length = 3)]
         public string BranchCode { get; set; }
 
-        [FixedWidthLineField(Start = 4, Length = 16)]
+        [FixedWidthLineField(Start = 4, Length = 8)]
         public string BranchName { get; set; }
 
-        [FixedWidthLineField(Start = 20, Length = 50)]
+        [FixedWidthLineField(Start = 12, Length = 25)]
         public string BranchAddress { get; set; }
 
-        [FixedWidthLineField(Start = 70, Length = 3)]
+        [FixedWidthLineField(Start = 37, Length = 3)]
         public string PhoneAreaCode { get; set; }
 
-        [FixedWidthLineField(Start = 73, Length = 8)]
+        [FixedWidthLineField(Start = 40, Length = 8)]
         public string PhoneNumber { get; set; }
 
-        [FixedWidthLineField(Start = 81, Length = 1)]
+        [FixedWidthLineField(Start = 48, Length = 1)]
         public string OperationalStatus { get; set; }
 
-        [FixedWidthLineField(Start = 82, Length = 3)]
+        [FixedWidthLineField(Start = 49, Length = 3)]
         public string ConvertedBranchCode { get; set; }
 
-        [FixedWidthLineField(Start = 85, Length = 10)]
+        [FixedWidthLineField(Start = 52, Length = 10)]
         public string Latitude { get; set; }
 
-        [FixedWidthLineField(Start = 95, Length = 11)]
+        [FixedWidthLineField(Start = 62, Length = 11)]
         public string Longitude { get; set; }
 
-        [FixedWidthLineField(Start = 106, Length = 6)]
+        [FixedWidthLineField(Start = 73, Length = 3)]
         public string City { get; set; }
 
-        [FixedWidthLineField(Start = 112, Length = 6)]
+        [FixedWidthLineField(Start = 76, Length = 3)]
         public string District { get; set; }
 
         public DefaultConfig GetDefaultConfig(int structureTypeId)
