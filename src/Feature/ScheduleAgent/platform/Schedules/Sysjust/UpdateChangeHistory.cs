bb4a9c0b402d5cf6e5adf5439d8c637dc793a669
Esther Lin <esther.lin@esi-tech.net>
2024-07-03 15:21:19 +0800

    Update: 新增清除ChangeHistory紀錄變動資料保留一個月排程

---
+++ src/Feature/ScheduleAgent/platform/Schedules/Sysjust/UpdateChangeHistory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Feature.Wealth.ScheduleAgent.Services;
+using Xcms.Sitecore.Foundation.QuartzSchedule;
+using Feature.Wealth.ScheduleAgent.Repositories;
+using Feature.Wealth.ScheduleAgent.Models.Sysjust;
+using Foundation.Wealth.Manager;
+using System.Data;
+
+namespace Feature.Wealth.ScheduleAgent.Schedules.Sysjust
+{
+    public class UpdateChangeHistory : SitecronAgentBase
+    {
+        protected override async Task Execute()
+        {
+            var _repository = new ProcessRepository(this.Logger);
+
+            string sql = "SELECT * FROM [ChangeHistory]";
+            var results = await DbManager.Custom.ExecuteIListAsync<ChangeHistory>(sql, null, CommandType.Text);
+
+            var newresults = results.Where(f => f.ModificationDate >= DateTime.Today.AddMonths(-1));
+
+            try
+            {
+                _repository.BulkInsertToNewDatabase(newresults, "[ChangeHistory]", sql);
+            }
+            catch (Exception ex)
+            {
+                this.Logger.Error(ex.Message, ex);
+                _repository.LogChangeHistory(DateTime.UtcNow, "ChangeHistory", ex.Message, " ", 0);
+            }
+
+        }
+    }
+}
