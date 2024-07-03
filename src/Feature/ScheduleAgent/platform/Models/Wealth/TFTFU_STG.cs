e0807964469c72d2eb2d3df99267e798c03b8469
Esther Lin <esther.lin@esi-tech.net>
2024-07-03 14:58:13 +0800

    Update: 新增TFTFU_STG排程 for 神機妙算與金速配使用

---
+++ src/Feature/ScheduleAgent/platform/Models/Wealth/TFTFU_STG.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feature.Wealth.ScheduleAgent.Models.Wealth
+{
+    public class TftfuStg
+    {
+        public string TFTFU_FUND_ID_CODE { get; set; }
+        public string TFTFU_SMART_TYPE { get; set; }
+        public string TFTFU_CTT_SHR_TYPE { get; set; }
+    }
+}
