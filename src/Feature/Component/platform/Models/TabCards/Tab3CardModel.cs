f0bee31404a2d883312e650802c0451dc3ee932f
Rosie <rosie.chiu@esi-tech.net>
2024-07-08 11:25:15 +0800

    Update: 移除點閱率demo資料(且暫時不須套用VisitCount)、更新文章連結屬性使用上稿

--- src/Feature/Component/platform/Models/TabCards/Tab3CardModel.cs
+++ src/Feature/Component/platform/Models/TabCards/Tab3CardModel.cs
@@ -11,10 +11,16 @@ namespace Feature.Wealth.Component.Models.TabCards
         public string CardDateTime1 => ((DateField)Datasource?.Fields[Tab3CardDatasource.Fields.Date1])?.GetLocalDateFieldValue()?.ToString(DateFormat);
         public string CardImageUrl1 { get; set; }
         public string ArticleLink1 { get; set; }
+        public string ArticleTarget1 { get; set; }
+        public string ArticleTitle1 { get; set; }
+        public int? ArticleVisitCount1 { get; set; } = null;
 
         public string CardDateTime2 => ((DateField)Datasource?.Fields[Tab3CardDatasource.Fields.Date2])?.GetLocalDateFieldValue()?.ToString(DateFormat);
         public string CardImageUrl2 { get; set; }
         public string ArticleLink2 { get; set; }
+        public string ArticleTarget2 { get; set; }
+        public string ArticleTitle2 { get; set; }
+        public int? ArticleVisitCount2 { get; set; } = null;
 
 
         public Tab3CardModel(Item item) : base(item)
@@ -25,10 +31,14 @@ namespace Feature.Wealth.Component.Models.TabCards
             }
 
             this.CardImageUrl1 = ItemUtils.ImageUrl(item, Tab3CardDatasource.Fields.Image1);
-            this.CardImageUrl2 = ItemUtils.ImageUrl(item, Tab3CardDatasource.Fields.Image2);
+            this.ArticleLink1 = ItemUtils.GeneralLink(item, Tab3CardDatasource.Fields.ArticleLink1)?.Url;
+            this.ArticleTarget1 = ItemUtils.GeneralLink(item, Tab3CardDatasource.Fields.ArticleLink1)?.Target;
+            this.ArticleTitle1 = ItemUtils.GeneralLink(item, Tab3CardDatasource.Fields.ArticleLink1)?.Title;
 
-            this.ArticleLink1 = ItemUtils.GeneralLink(item, Tab3CardDatasource.Fields.ArticleLink1)?.Url ?? "#";
-            this.ArticleLink2 = ItemUtils.GeneralLink(item, Tab3CardDatasource.Fields.ArticleLink2)?.Url ?? "#";
+            this.CardImageUrl2 = ItemUtils.ImageUrl(item, Tab3CardDatasource.Fields.Image2);
+            this.ArticleLink2 = ItemUtils.GeneralLink(item, Tab3CardDatasource.Fields.ArticleLink2)?.Url;
+            this.ArticleTarget2 = ItemUtils.GeneralLink(item, Tab3CardDatasource.Fields.ArticleLink2)?.Target;
+            this.ArticleTitle2 = ItemUtils.GeneralLink(item, Tab3CardDatasource.Fields.ArticleLink2)?.Title;
         }
     }
 }
