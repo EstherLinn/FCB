1299fb44cf586ee7d88415421245741186000c76
Michael Lee <michael.lee@esi-tech.net>
2024-07-08 14:42:00 +0800

    Add : 新增教育基金試算

---
+++ src/Feature/Component/platform/Models/Calculate/CalculateTemplates.cs
@@ -0,0 +1,42 @@
+﻿using Sitecore.Data;
+
+namespace Feature.Wealth.Component.Models.Calculate
+{
+    public class Template
+    {
+        public struct Calculate
+        {
+            public static readonly ID Id = new ID("{E2B44A81-7D97-4231-8932-78381DC5776F}");
+
+            public struct Fields
+            {
+                public static readonly ID MainTitle = new ID("{BBAD1259-2A5C-406C-ACA1-7723E8DE27D6}");
+                public static readonly ID MainContent = new ID("{E35BD6E6-8C20-49A5-B929-5E4FD5A3F33E}");
+                public static readonly ID Image = new ID("{8BB3CEFC-DF8D-411D-BD60-297DDF34538A}");
+                public static readonly ID AnticipatedInvestmentTipTitle = new ID("{7F9176BC-7967-4448-BAE2-2FA8CE706CEE}");
+                public static readonly ID AnticipatedInvestmentTipContent = new ID("{593B2249-BED8-4774-967B-0F44799F55C0}");
+                public static readonly ID LifeExpectancyTipTitle = new ID("{5EC4170A-8B59-4D02-8713-30C250B97C4B}");
+                public static readonly ID LifeExpectancyTipContent = new ID("{A67FD7D3-CA7E-4B9C-A806-9EFE906DA0E9}");
+                public static readonly ID RetirementExpensesTipTitle = new ID("{874941F5-6971-4A3C-9789-FCE41B7559FA}");
+                public static readonly ID RetirementExpensesTipContent = new ID("{9CC4FC0B-2F69-43E5-9AD2-3470F0B4611A}");
+                public static readonly ID EstimatedPensionTipTitle = new ID("{2204FCD4-B159-44A0-9193-D77E81D558A0}");
+                public static readonly ID EstimatedPensionTipContent = new ID("{CE40CEBF-BBC6-45D4-AD9C-FF6298E22551}");
+                public static readonly ID SuccessImage = new ID("{928BA457-1FA5-46D7-9A6B-E98B592DE925}");
+                public static readonly ID SuccessContent = new ID("{BB7D3CDD-33AB-4F8A-900D-169015C03CAE}");
+                public static readonly ID UnsuccessfulImage = new ID("{AF7DE8E1-36DA-49AD-AE76-50C3621F8EED}");
+                public static readonly ID UnsuccessfulContent = new ID("{DB09C138-CB42-4485-992C-D5A01240BE20}");
+                public static readonly ID ExpectedReturnRemarks = new ID("{94FFECEE-3112-4AE4-8DED-B94870DC3885}");
+                public static readonly ID DefaultRiskAttributes = new ID("{54721E54-E73F-480E-B5D9-1B4A03CD6594}");
+                public static readonly ID StockAllocation = new ID("{84CB4BBC-6E9F-4F5B-AB75-6A6E76F54A1C}");
+                public static readonly ID BondAllocation = new ID("{FEF9B7DA-2A2F-4899-B36C-3804C676C969}");
+                public static readonly ID CurrencyAllocation = new ID("{0F0DF087-72F2-4097-9C22-C853CB0885B5}");
+                public static readonly ID Notice = new ID("{F6E26787-A45D-4424-8C7A-82DEF7FBBD7A}");
+                public static readonly ID RemoteConsultationTitle = new ID("{FFEE7E90-EFEC-42EE-B102-2C60766C12E1}");
+                public static readonly ID RemoteConsultationContent = new ID("{07158773-FDEB-4207-8AF3-660A0F51D9D3}");
+                public static readonly ID RemoteConsultationButtonText = new ID("{39C7F9EE-E9B2-47F1-B81F-54A262B295B8}");
+                public static readonly ID RemoteConsultationButtonLink = new ID("{57B825FF-6BBA-4384-8CC8-27661DDE841E}");
+                public static readonly ID RemoteConsultationImage = new ID("{7DA96E92-17D2-439B-927B-094232C2E3E0}");
+            }
+        }
+    }
+}
---
+++ src/Feature/Component/platform/Repositories/CalculateRepository.cs
@@ -0,0 +1,56 @@
+﻿using Feature.Wealth.Component.Models.Calculate;
+using Sitecore.Mvc.Presentation;
+using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
+
+namespace Feature.Wealth.Component.Repositories
+{
+    public class CalculateRepository
+    {
+        public CalculateModel GetCalculateModel()
+        {
+            var dataSource = RenderingContext.CurrentOrNull?.ContextItem;
+
+            if (dataSource == null || dataSource.TemplateID != Template.Calculate.Id)
+            {
+                return null;
+            }
+
+            CalculateModel model = new CalculateModel();
+
+            model.Datasource = dataSource;
+            model.MainTitle = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.MainTitle);
+            model.MainContent = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.MainContent);
+            model.Image = ItemUtils.ImageUrl(dataSource, Template.Calculate.Fields.Image);
+            model.AnticipatedInvestmentTipTitle = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.AnticipatedInvestmentTipTitle);
+            model.AnticipatedInvestmentTipContent = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.AnticipatedInvestmentTipContent);
+            model.LifeExpectancyTipTitle = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.LifeExpectancyTipTitle);
+            model.LifeExpectancyTipContent = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.LifeExpectancyTipContent);
+            model.RetirementExpensesTipTitle = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.RetirementExpensesTipTitle);
+            model.RetirementExpensesTipContent = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.RetirementExpensesTipContent);
+            model.EstimatedPensionTipTitle = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.EstimatedPensionTipTitle);
+            model.EstimatedPensionTipContent = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.EstimatedPensionTipContent);
+            model.SuccessImage = ItemUtils.ImageUrl(dataSource, Template.Calculate.Fields.SuccessImage);
+            model.SuccessContent = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.SuccessContent);
+            model.UnsuccessfulImage = ItemUtils.ImageUrl(dataSource, Template.Calculate.Fields.UnsuccessfulImage);
+            model.UnsuccessfulContent = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.UnsuccessfulContent);
+            model.ExpectedReturnRemarks = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.ExpectedReturnRemarks);
+
+            var defaultRiskAttributes = ItemUtils.GetReferenceFieldItem(dataSource, Template.Calculate.Fields.DefaultRiskAttributes);
+            model.DefaultRiskAttributes = defaultRiskAttributes != null
+            ? ItemUtils.GetFieldValue(defaultRiskAttributes, ComponentTemplates.DropdownOption.Fields.OptionValue)
+            : "2";
+
+            model.StockAllocation = ItemUtils.GetInteger(dataSource, Template.Calculate.Fields.StockAllocation);
+            model.BondAllocation = ItemUtils.GetInteger(dataSource, Template.Calculate.Fields.BondAllocation);
+            model.CurrencyAllocation = ItemUtils.GetInteger(dataSource, Template.Calculate.Fields.CurrencyAllocation);
+            model.Notice = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.Notice);
+            model.RemoteConsultationTitle = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.RemoteConsultationTitle);
+            model.RemoteConsultationContent = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.RemoteConsultationContent);
+            model.RemoteConsultationButtonText = ItemUtils.GetFieldValue(dataSource, Template.Calculate.Fields.RemoteConsultationButtonText);
+            model.RemoteConsultationButtonLink = ItemUtils.GeneralLink(dataSource, Template.Calculate.Fields.RemoteConsultationButtonLink).Url;
+            model.RemoteConsultationImage = ItemUtils.ImageUrl(dataSource, Template.Calculate.Fields.RemoteConsultationImage);
+
+            return model;
+        }
+    }
+}
