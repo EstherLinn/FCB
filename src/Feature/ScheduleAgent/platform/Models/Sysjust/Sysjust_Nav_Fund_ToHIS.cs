4adcdbfc340ce6d68b1f05df5fc59de573ebbd7d
Esther Lin <esther.lin@esi-tech.net>
2024-07-08 15:30:34 +0800

    Update: 新增Sysjust-Nav-Fund.txt 到 NAVFUND_HIS、更新Sysjust_Nav_Fund保留31天的淨值資料

---
+++ src/Feature/ScheduleAgent/platform/Models/Sysjust/Sysjust_Nav_Fund_ToHIS.cs
@@ -0,0 +1,32 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace Feature.Wealth.ScheduleAgent.Models.Sysjust
+{
+    /// <summary>
+    /// 基金歷史淨值，檔案名稱：SYSJUST-FUNDNAV-HIS.txt
+    /// </summary>
+    [Delimiter(";@")]
+    [HasHeaderRecord(false)]
+    internal class SysjustNavFundToHis
+    {
+        /// <summary>
+        /// 一銀代碼
+        /// </summary>
+        [Index(0)]
+        public string FirstBankCode { get; set; }
+
+        /// <summary>
+        /// 日期
+        /// </summary>
+        [Index(1)]
+        [NullValues("", "NULL", null)]
+        public string Date { get; set; }
+
+        /// <summary>
+        /// 淨值
+        /// </summary>
+        [Index(2)]
+        [NullValues("", "NULL", null)]
+        public string NetAssetValue { get; set; }
+    }
+}
