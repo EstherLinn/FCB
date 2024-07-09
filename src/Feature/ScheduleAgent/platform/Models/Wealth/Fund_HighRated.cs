0671b3e21599b698028b9493d29a902e608d49fb
Esther Lin <esther.lin@esi-tech.net>
2024-07-09 15:00:40 +0800

    Fix: 排程bug

--- src/Feature/ScheduleAgent/platform/Models/Wealth/Fund_HighRated.cs
+++ src/Feature/ScheduleAgent/platform/Models/Wealth/Fund_HighRated.cs
@@ -5,8 +5,8 @@ namespace Feature.Wealth.ScheduleAgent.Models.Wealth
     /// <summary>
     /// 好評基金 (wms_fund_top15_m_mf)，檔案名稱：wms_fund_top15_m_mf.txt
     /// </summary>
-    [Delimiter(";")]
-    [HasHeaderRecord(false)]
+    [Delimiter(",")]
+    [HasHeaderRecord(true)]
     public class FundHighRated
     {
         public string ProductCode { get; set; }
