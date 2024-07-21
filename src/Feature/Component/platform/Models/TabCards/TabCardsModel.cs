2ddbc10828eead363c2c08551c98dec19fe07a8c
Mice.Lin <mice.lin@esi-tech.net>
2024-07-21 10:12:59 +0800

    #4142 B03.首頁Tab三牌卡元件無法另開視窗

--- src/Feature/Component/platform/Models/TabCards/TabCardsModel.cs
+++ src/Feature/Component/platform/Models/TabCards/TabCardsModel.cs
@@ -1,8 +1,6 @@
 ﻿using Feature.Wealth.Component.Models.FundDetail;
 using Newtonsoft.Json;
-using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +14,7 @@ namespace Feature.Wealth.Component.Models.TabCards
         public string Title1 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Title1);
         public string ImageUrl1 { get; set; }
         public string BannerLink1 { get; set; }
+        public string BannerTarget1 { get; set; }
         public bool IsBlackFont1 => ItemUtils.IsChecked(Datasource, _TabCard.Fields.IsBlackFont1);
         public string Description1 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Description1);
 
@@ -23,42 +22,49 @@ namespace Feature.Wealth.Component.Models.TabCards
         public string Title2 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Title2);
         public string ImageUrl2 { get; set; }
         public string BannerLink2 { get; set; }
+        public string BannerTarget2 { get; set; }
         public bool IsBlackFont2 => ItemUtils.IsChecked(Datasource, _TabCard.Fields.IsBlackFont2);
         public string Description2 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Description2);
 
         public string Title3 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Title3);
         public string ImageUrl3 { get; set; }
         public string BannerLink3 { get; set; }
+        public string BannerTarget3 { get; set; }
         public bool IsBlackFont3 => ItemUtils.IsChecked(Datasource, _TabCard.Fields.IsBlackFont3);
         public string Description3 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Description3);
 
         public string Title4 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Title4);
         public string ImageUrl4 { get; set; }
         public string BannerLink4 { get; set; }
+        public string BannerTarget4 { get; set; }
         public bool IsBlackFont4 => ItemUtils.IsChecked(Datasource, _TabCard.Fields.IsBlackFont4);
         public string Description4 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Description4);
 
         public string Title5 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Title5);
         public string ImageUrl5 { get; set; }
         public string BannerLink5 { get; set; }
+        public string BannerTarget5 { get; set; }
         public bool IsBlackFont5 => ItemUtils.IsChecked(Datasource, _TabCard.Fields.IsBlackFont5);
         public string Description5 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Description5);
 
         public string Title6 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Title6);
         public string ImageUrl6 { get; set; }
         public string BannerLink6 { get; set; }
+        public string BannerTarget6 { get; set; }
         public bool IsBlackFont6 => ItemUtils.IsChecked(Datasource, _TabCard.Fields.IsBlackFont6);
         public string Description6 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Description6);
 
         public string Title7 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Title7);
         public string ImageUrl7 { get; set; }
         public string BannerLink7 { get; set; }
+        public string BannerTarget7 { get; set; }
         public bool IsBlackFont7 => ItemUtils.IsChecked(Datasource, _TabCard.Fields.IsBlackFont7);
         public string Description7 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Description7);
 
         public string Title8 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Title8);
         public string ImageUrl8 { get; set; }
         public string BannerLink8 { get; set; }
+        public string BannerTarget8 { get; set; }
         public bool IsBlackFont8 => ItemUtils.IsChecked(Datasource, _TabCard.Fields.IsBlackFont8);
         public string Description8 => ItemUtils.GetFieldValue(Datasource, _TabCard.Fields.Description8);
 
@@ -90,15 +96,31 @@ namespace Feature.Wealth.Component.Models.TabCards
             this.ImageUrl7 = ItemUtils.ImageUrl(item, _TabCard.Fields.Banner7);
             this.ImageUrl8 = ItemUtils.ImageUrl(item, _TabCard.Fields.Banner8);
 
-            this.BannerLink1 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink1)?.Url;
-            this.BannerLink2 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink2)?.Url;
-            this.BannerLink3 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink3)?.Url;
-            this.BannerLink4 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink4)?.Url;
-            this.BannerLink5 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink5)?.Url;
-            this.BannerLink6 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink6)?.Url;
-            this.BannerLink7 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink7)?.Url;
-            this.BannerLink8 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink8)?.Url;
+            var link1 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink1);
+            var link2 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink2);
+            var link3 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink3);
+            var link4 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink4);
+            var link5 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink5);
+            var link6 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink6);
+            var link7 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink7);
+            var link8 = ItemUtils.GeneralLink(item, _TabCard.Fields.BannerLink8);
 
+            this.BannerLink1 = link1?.Url;
+            this.BannerLink2 = link2?.Url;
+            this.BannerLink3 = link3?.Url;
+            this.BannerLink4 = link4?.Url;
+            this.BannerLink5 = link5?.Url;
+            this.BannerLink6 = link6?.Url;
+            this.BannerLink7 = link7?.Url;
+            this.BannerLink8 = link8?.Url;
+            this.BannerTarget1 = link1?.Target;
+            this.BannerTarget2 = link2?.Target;
+            this.BannerTarget3 = link3?.Target;
+            this.BannerTarget4 = link4?.Target;
+            this.BannerTarget5 = link5?.Target;
+            this.BannerTarget6 = link6?.Target;
+            this.BannerTarget7 = link7?.Target;
+            this.BannerTarget8 = link8?.Target;
 
             this.FundIDList = ItemUtils.GetMultiLineText(item, _TabCard.Fields.FundIDList)?.Take(3).ToList() ?? new List<string>();
             this.FundIDListHtmlString = new HtmlString(JsonConvert.SerializeObject(this.FundIDList));
