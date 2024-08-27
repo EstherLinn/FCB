5910211e11dfc8aae5d6afbb2593ad28b63347ea
Andrew Wu <andrew.wu@esi-tech.net>
2024-08-27 15:22:38 +0800

    增加紀錄log

--- src/Feature/Component/platform/Repositories/OctonApiRespository.cs
+++ src/Feature/Component/platform/Repositories/OctonApiRespository.cs
@@ -14,6 +14,7 @@ namespace Feature.Wealth.Component.Repositories
     public class OctonApiRespository
     {
         private readonly string _route = Settings.GetSetting("Octon");
+        private readonly ILog _log = Logger.Api;
 
         /// <summary>
         /// 取得視訊連結
@@ -23,32 +24,47 @@ namespace Feature.Wealth.Component.Repositories
         public JObject GetWebURL(OctonRequestData octonRequestData)
         {
             JObject result = null;
-            var request = _route.
-            AppendPathSegments("mmccmedia", "GetWebURL").
-            SetQueryParams(new
-            {
-                Tenant = "YJI304",
-                SysCode = "Octon",
-                dnis = octonRequestData.dnis,
-                Date = octonRequestData.Date,
-                Start = octonRequestData.Start,
-                End = octonRequestData.End,
-                EmployeeCode = octonRequestData.EmployeeCode,
-                EmployeeName = octonRequestData.EmployeeName,
-                BranchCode = octonRequestData.BranchCode,
-                BranchName = octonRequestData.BranchName,
-                BranchPhone = octonRequestData.BranchPhone
-            }).
-            WithHeader("Authorization", octonRequestData.Authorization).
-            WithHeader("ContentType", "application/x-www-form-urlencoded;charset=utf-8").
-            AllowAnyHttpStatus().
-            PostAsync().
-            ReceiveString().Result;
 
-            if (!string.IsNullOrEmpty(request))
+            try
             {
-                result = JObject.Parse(request);
+                var request = _route.
+                AppendPathSegments("mmccmedia", "GetWebURL").
+                SetQueryParams(new
+                {
+                    Tenant = "YJI304",
+                    SysCode = "Octon",
+                    dnis = octonRequestData.dnis,
+                    Date = octonRequestData.Date,
+                    Start = octonRequestData.Start,
+                    End = octonRequestData.End,
+                    EmployeeCode = octonRequestData.EmployeeCode,
+                    EmployeeName = octonRequestData.EmployeeName,
+                    BranchCode = octonRequestData.BranchCode,
+                    BranchName = octonRequestData.BranchName,
+                    BranchPhone = octonRequestData.BranchPhone
+                }).
+                WithHeader("Authorization", octonRequestData.Authorization).
+                WithHeader("ContentType", "application/x-www-form-urlencoded;charset=utf-8").
+                AllowAnyHttpStatus().
+                PostAsync().
+                ReceiveString().Result;
+
+                if (!string.IsNullOrEmpty(request))
+                {
+                    result = JObject.Parse(request);
+                }
             }
+            catch (FlurlHttpException ex)
+            {
+                var status = ex.StatusCode;
+                var resp = ex.GetResponseStringAsync().Result;
+                this._log.Error($"Error returned from {ex.Call.Request.Url} {Environment.NewLine}[Message] {ex.Message} {Environment.NewLine}[StatusCode] {status}{Environment.NewLine}[Response] {resp}");
+            }
+            catch (Exception ex)
+            {
+                this._log.Error(ex);
+            }
+
             return result;
         }
     }
