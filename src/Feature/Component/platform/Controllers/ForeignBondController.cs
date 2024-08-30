3fb5eed0d44852f607b7baa19db5ccf661b6f336
shuttle.hong <shuttle.hong@esi-tech.net>
2024-08-30 16:07:28 +0800

    Add:國外債券:債券梯、啞鈴式投資

---
+++ src/Feature/Component/platform/Controllers/ForeignBondController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class ForeignBondController : Controller
+    {
+
+        /// <summary>
+        ///  債券梯
+        /// </summary>
+        public ActionResult BondLadder()
+        {
+            return View("/Views/Feature/Wealth/Component/ForeignBond/BondLadder.cshtml");
+        }
+
+        /// <summary>
+        ///  啞鈴式投資
+        /// </summary>
+        public ActionResult BarbellApproach()
+        {
+            return View("/Views/Feature/Wealth/Component/ForeignBond/BarbellApproach.cshtml");
+        }
+    }
+}
