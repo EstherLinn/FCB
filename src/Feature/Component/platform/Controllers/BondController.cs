459e110ddd6f79f62171a6a9608f5b2f4ce9aa40
Andrew Wu <andrew.wu@esi-tech.net>
2024-09-04 19:07:36 +0800

    修改列表搜尋跟欄位

--- src/Feature/Component/platform/Controllers/BondController.cs
+++ src/Feature/Component/platform/Controllers/BondController.cs
@@ -31,6 +31,54 @@ namespace Feature.Wealth.Component.Controllers
             var hotProductTags = ItemUtils.GetMultiListValueItems(item, Template.Bond.Fields.HotProduct);
             var products = hotProductTags.Select(f => ItemUtils.GetFieldValue(f, Template.BondTag.Fields.TagName)).ToList();
 
+            var docs = ItemUtils.GetChildren(ItemUtils.GetItem(Template.DocFolder.Id));
+            var dic_docs = new Dictionary<string, List<string>>();
+
+            foreach (var doc in docs)
+            {
+                var key = doc.Name.Substring(0, 4);
+
+                if (dic_docs.ContainsKey(key) == false)
+                {
+                    var paths = new List<string>
+                    {
+                        doc.Paths.Path
+                    };
+
+                    dic_docs.Add(key, paths);
+                }
+                else
+                {
+                    var paths = dic_docs[key];
+                    paths.Add(doc.Paths.Path);
+                    dic_docs[key] = paths;
+                }
+            }
+
+            for (int i = 0; i < bondList.Count; i++)
+            {
+                var bondCode = bondList[i].BondCode;
+                if (dic_docs.ContainsKey(bondCode))
+                {
+                    bondList[i].DocPaths = dic_docs[bondList[i].BondCode];
+                }
+
+                if(bondList[i].DocPaths.Count > 0)
+                {
+                    var docString = string.Empty;
+
+                    foreach(var path in bondList[i].DocPaths)
+                    {
+                        docString += $@"<a href=""{path}"" target=""_blank"" class=""o-prefixLink o-prefixLink--insidebox o-prefixLink--pdf t-bold""></a>";
+                    }
+
+                    bondList[i].DocString = docString;
+                }
+
+            }
+
+
+
             var model = new BondModel
             {
                 Item = item,
@@ -40,6 +88,7 @@ namespace Feature.Wealth.Component.Controllers
                 HotProductTags = products,
                 CurrencyList = bondList.OrderBy(i => i.CurrencyCode).Select(i => i.CurrencyName).Distinct(),
                 PaymentFrequencyList = bondList.OrderBy(i => i.PaymentFrequency).Select(i => i.PaymentFrequencyName).Distinct(),
+                BondClassList = bondList.OrderBy(i => i.BondClass).Select(i => i.BondClass).Distinct(),
                 IssuerList = bondList.OrderBy(i => i.Issuer).Select(i => i.Issuer).Distinct(),
             };
 
