c630c17956b62720c67d4ff5359a9cea2d7306e2
Sean Tong <sean.tong@esi-tech.net>
2024-09-24 17:58:17 +0800

    基金搜尋頁查無資料時，顯示你可能會喜歡元件

---
+++ src/Feature/Component/platform/Controllers/InterestedFundController.cs
@@ -0,0 +1,26 @@
+﻿using Feature.Wealth.Component.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Xcms.Sitecore.Foundation.Basic.Extensions;
+
+namespace Feature.Wealth.Component.Controllers
+{
+    public class InterestedFundController : Controller
+    {
+        private readonly InterestedFundRepository _interestedFundRepository = new InterestedFundRepository ();
+        public ActionResult Index()
+        {
+            return View("/Views/Feature/Wealth/Component/InterestedFund/InterestedFund.cshtml");
+        }
+
+        [HttpPost]
+        public ActionResult GetInterestedFund()
+        {
+            return new JsonNetResult(_interestedFundRepository.GetFundData());
+        }
+    }
+}
---
+++ src/Feature/Component/platform/Models/InterestedFund/InterestedFundModle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feature.Wealth.Component.Models.InterestedFund
+{
+    public class InterestedFundModle
+    {
+        public IList<Fund> Funds {  get; set; }
+    }
+
+    public class Fund
+    {
+        public string DetailUrl { get; set; }
+        public string FundName { get; set; }
+        public string FundId { get; set; }
+        public string RateOfRetuen { get; set; }
+        public string FocusButtonHtml { get; set; }
+        public string CompareButtonHtml { get; set; }
+        public string SubscribeButtonHtml { get; set; }
+
+    }
+
+    //從DB獲取資料
+    public class FundModel
+    {
+        public string FundId { get; set; }
+        public string FundName { get; set; }
+        public decimal? OneMonthReturnOriginalCurrency { get; set; }
+        public string AvailabilityStatus { get; set; }
+        public string OnlineSubscriptionAvailability { get; set; }
+    }
+
+
+}
