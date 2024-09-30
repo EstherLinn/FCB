b866efffd10e346279286ad06a54963b4fb95ca5
Joyce Chen <joyce.chen@esi-tech.net>
2024-09-30 18:23:13 +0800

    新增Keepalive

---
+++ src/Feature/Toolkit/platform/Areas/Tools/Controllers/KeepaliveController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Feature.Wealth.Toolkit.Areas.Tools.Controllers
+{
+    public class KeepaliveController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("~/Areas/Tools/Views/CacheManager/keepalive.cshtml");
+        }
+    }
+}
