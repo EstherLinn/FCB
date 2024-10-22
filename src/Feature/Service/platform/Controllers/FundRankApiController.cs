0ab1b783e5e5a6be75e10ae6b2fb03531ea5a609
Sean Tong <sean.tong@esi-tech.net>
2024-10-22 10:15:26 +0800

    新增基金排行API，含人氣、得獎、國內境外績效 排行

---
+++ src/Feature/Service/platform/Controllers/FundRankApiController.cs
@@ -0,0 +1,26 @@
+﻿using Feature.Wealth.Service.Repositories;
+using System.Web.Mvc;
+using Xcms.Sitecore.Foundation.Basic.Extensions;
+using static Feature.Wealth.Service.Models.FundRankApi.FundRankApiModel;
+
+namespace Feature.Wealth.Service.Controllers
+{
+    public class FundRankApiController : JsonNetController
+    {
+        private readonly FundRankApiRepository _fundRankApiRepository = new FundRankApiRepository();
+
+        [HttpPost]
+        public ActionResult GetFundRank(string data = "6")
+        {
+            var result = new ReturnFundSet
+            {
+                DomesticPerformanceRankFunds = _fundRankApiRepository.GetPerformanceFundRank("D", data),
+                ForeignPerformanceRankFunds = _fundRankApiRepository.GetPerformanceFundRank("O", data),
+                PopularityFunds = _fundRankApiRepository.GetPopularityFunds(data),
+                AwardedFunds = _fundRankApiRepository.GetAwardFunds(data)
+            };
+
+            return new JsonNetResult(result);
+        }
+    }
+}
