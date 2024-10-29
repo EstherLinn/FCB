8b5f68cfdbe7e14194385833868484e0b888fb4d
Jenny Yeh <jenny.yeh@esi-tech.net>
2024-10-29 15:59:52 +0800

    AuthenticationHistory 資料表擴增欄位
    - 新增欄位：FullName, DepartmentName, Roles, IP
    - 新增 FCB user profile 自定義屬性 "Roles"，當 domain 為 fcb 時才會設置

--- src/Feature/Account/platform/Pipelines/Owin.CookieAuthentication/SignOut/LogoutTracker.cs
+++ src/Feature/Account/platform/Pipelines/Owin.CookieAuthentication/SignOut/LogoutTracker.cs
@@ -1,7 +1,12 @@
-﻿using Foundation.Wealth.Manager;
+﻿using Feature.Wealth.Account.Models.SingleSignOn;
+using Foundation.Wealth.Helper;
+using Foundation.Wealth.Manager;
 using Sitecore.Diagnostics;
 using Sitecore.Owin.Authentication.Pipelines.CookieAuthentication.SignOut;
+using Sitecore.Security.Accounts;
+using System;
 using System.Data;
+using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
 
 namespace Feature.Wealth.Account.Pipelines.Owin.CookieAuthentication.SignOut
 {
@@ -10,16 +15,50 @@ namespace Feature.Wealth.Account.Pipelines.Owin.CookieAuthentication.SignOut
         public override void Process(SignOutArgs args)
         {
             Assert.ArgumentNotNull(args, "args");
+            var user = GetUser(args);
+
+            var domain = DomainUtils.GetDomain(SsoDomain.fcb);
+            FirstBankUserProfile firstBankUser = new FirstBankUserProfile();
+
+            // Check if the domain is FCB
+            if (user.GetDomainName().Equals(domain?.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                firstBankUser.Roles = user.Profile.GetCustomProperty("Roles").Replace(Environment.NewLine, ";");
+                firstBankUser.DepartmentName = user.Profile.GetCustomProperty("DepartmentName");
+            }
 
             DbManager.Custom.ExecuteNonQuery(
-                @"INSERT INTO AuthenticationHistory 
-                        (UserName, Action)
-                        VALUES(@UserName, @Action);",
+                @"INSERT INTO AuthenticationHistory
+                    (UserName, Action, FullName, DepartmentName, Roles, IP)
+                    VALUES (@UserName, @Action, @FullName, @DepartmentName, @Roles, @IP);",
                 new
                 {
                     args.UserName,
-                    Action = "Logout"
+                    Action = "Logout",
+                    FullName = user.Profile.FullName,
+                    DepartmentName = firstBankUser.DepartmentName,
+                    Roles = firstBankUser.Roles,
+                    IP = IPHelper.GetIPAddress()
                 }, CommandType.Text);
+        }
+
+        /// <summary>
+        /// Get the user
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private User GetUser(SignOutArgs args)
+        {
+            User tempUser = null;
+            // Get the user from the claims identity
+            string userName = args.UserName;
+            if (!string.IsNullOrEmpty(userName))
+            {
+                // True will make sure that the user is authenticated
+                tempUser = User.FromName(userName, true);
+            }
+
+            return tempUser;
         }
     }
 }
--- src/Feature/Account/platform/Pipelines/Owin.CookieAuthentication/SignedIn/LoginTracker.cs
+++ src/Feature/Account/platform/Pipelines/Owin.CookieAuthentication/SignedIn/LoginTracker.cs
@@ -1,10 +1,14 @@
-﻿using Foundation.Wealth.Manager;
+﻿using Feature.Wealth.Account.Models.SingleSignOn;
+using Foundation.Wealth.Helper;
+using Foundation.Wealth.Manager;
 using Sitecore.Diagnostics;
 using Sitecore.Owin.Authentication.Pipelines.CookieAuthentication.SignedIn;
 using Sitecore.Security.Accounts;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
 
 namespace Feature.Wealth.Account.Pipelines.Owin.CookieAuthentication.SignedIn
 {
@@ -14,17 +18,32 @@ namespace Feature.Wealth.Account.Pipelines.Owin.CookieAuthentication.SignedIn
         {
             Assert.ArgumentNotNull(args, "args");
             var user = GetUser(args);
+
             if (user != null && user.IsAuthenticated)
             {
+                var domain = DomainUtils.GetDomain(SsoDomain.fcb);
+                FirstBankUserProfile firstBankUser = new FirstBankUserProfile();
+
+                // Check if the domain is FCB
+                if (user.GetDomainName().Equals(domain?.Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    firstBankUser.Roles = user.Profile.GetCustomProperty("Roles").Replace(Environment.NewLine, ";");
+                    firstBankUser.DepartmentName = user.Profile.GetCustomProperty("DepartmentName");
+                }
+
                 LogUserRoles(user);
                 DbManager.Custom.ExecuteNonQuery(
-                    @"INSERT INTO AuthenticationHistory 
-                        (UserName, Action)
-                        VALUES(@UserName, @Action);",
+                    @"INSERT INTO AuthenticationHistory
+                        (UserName, Action, FullName, DepartmentName, Roles, IP)
+                        VALUES (@UserName, @Action, @FullName, @DepartmentName, @Roles, @IP);",
                     new
                     {
                         UserName = user.Name,
-                        Action = "Login"
+                        Action = "Login",
+                        FullName = user.Profile.FullName,
+                        DepartmentName = firstBankUser.DepartmentName,
+                        Roles = firstBankUser.Roles,
+                        IP = IPHelper.GetIPAddress()
                     }, CommandType.Text);
             }
             else
@@ -32,7 +51,6 @@ namespace Feature.Wealth.Account.Pipelines.Owin.CookieAuthentication.SignedIn
                 Log.Error("User no authenticated: " + args.User?.UserName, this);
             }
         }
-
 
         /// <summary>
         /// This will return the user based on identity
@@ -53,6 +71,7 @@ namespace Feature.Wealth.Account.Pipelines.Owin.CookieAuthentication.SignedIn
 
             return tempUser;
         }
+
         private void LogUserRoles(User user)
         {
             Log.Info($"Logined User Name: {user.Name}, Role Count: {user.Roles?.Count()}, Roles: {string.Join("|", user.Roles.Select(r => r.DisplayName))}", this);
