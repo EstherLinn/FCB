2326f28341e43f7f364a3a83f8cf92d0093b752f
Jenny Yeh <jenny.yeh@esi-tech.net>
2024-10-30 17:00:57 +0800

    Update：登出 pipeline 改由其他 processor 處理
    - 因登出會重複觸發兩次的情況， 建立新的 Logout Pipeline 配置

---
+++ src/Feature/Account/platform/Pipelines/SignOutTracker.cs
@@ -0,0 +1,65 @@
+﻿using Feature.Wealth.Account.Models.SingleSignOn;
+using Foundation.Wealth.Helper;
+using Foundation.Wealth.Manager;
+using Sitecore;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.Logout;
+using Sitecore.Security.Accounts;
+using System;
+using System.Data;
+using Xcms.Sitecore.Foundation.Basic.SitecoreExtensions;
+
+namespace Feature.Wealth.Account.Pipelines
+{
+    public class SignOutTracker
+    {
+        public void Process(LogoutArgs args)
+        {
+            Assert.ArgumentNotNull(args, nameof(args));
+
+            var user = GetUser();
+            var domain = DomainUtils.GetDomain(SsoDomain.fcb);
+            FirstBankUserProfile firstBankUser = new FirstBankUserProfile();
+
+            // Check if the domain is FCB
+            if (Context.User.Domain.Name.Equals(domain?.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                firstBankUser.Roles = user.Profile.GetCustomProperty("Roles").Replace(Environment.NewLine, ";");
+                firstBankUser.DepartmentName = user.Profile.GetCustomProperty("DepartmentName");
+            }
+
+            DbManager.Custom.ExecuteNonQuery(
+                @"INSERT INTO AuthenticationHistory
+                    (UserName, Action, FullName, DepartmentName, Roles, IP)
+                    VALUES (@UserName, @Action, @FullName, @DepartmentName, @Roles, @IP);",
+                new
+                {
+                    UserName = user.Name,
+                    Action = "Logout",
+                    FullName = user.Profile.FullName,
+                    DepartmentName = firstBankUser.DepartmentName,
+                    Roles = firstBankUser.Roles,
+                    IP = IPHelper.GetIPAddress()
+                }, CommandType.Text);
+        }
+
+        /// <summary>
+        /// Get the user
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private User GetUser()
+        {
+            User tempUser = null;
+            // Get the user from the claims identity
+            string userName = Context.User.Name;
+            if (!string.IsNullOrEmpty(userName))
+            {
+                // True will make sure that the user is authenticated
+                tempUser = User.FromName(userName, true);
+            }
+
+            return tempUser;
+        }
+    }
+}
