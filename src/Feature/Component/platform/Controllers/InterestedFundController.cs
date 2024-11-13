e9335561d0be8414eb319e2df3148614b212c414
Sean Tong <sean.tong@esi-tech.net>
2024-11-13 18:09:23 +0800

    優化 你可能會喜歡元件

--- src/Feature/Component/platform/Controllers/InterestedFundController.cs
+++ src/Feature/Component/platform/Controllers/InterestedFundController.cs
@@ -1,8 +1,7 @@
 ﻿using Feature.Wealth.Component.Repositories;
+using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Runtime.Caching;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Xcms.Sitecore.Foundation.Basic.Extensions;
@@ -11,7 +10,10 @@ namespace Feature.Wealth.Component.Controllers
 {
     public class InterestedFundController : Controller
     {
-        private readonly InterestedFundRepository _interestedFundRepository = new InterestedFundRepository ();
+        private readonly InterestedFundRepository _interestedFundRepository = new InterestedFundRepository();
+        private readonly DjMoneyApiRespository _djMoneyApiRespository = new DjMoneyApiRespository();
+        private readonly MemoryCache _cache = MemoryCache.Default;
+
         public ActionResult Index()
         {
             return View("/Views/Feature/Wealth/Component/InterestedFund/InterestedFund.cshtml");
@@ -20,7 +22,23 @@ namespace Feature.Wealth.Component.Controllers
         [HttpPost]
         public ActionResult GetInterestedFund()
         {
-            return new JsonNetResult(_interestedFundRepository.GetFundData());
+            return new JsonNetResult(_interestedFundRepository.GetOrSetCacheFundData());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> GetCloseYearPerformance(string fundID)
+        {
+            string cacheKey = $"CloseYearPerformance_{fundID.ToUpper()}";
+            var cachedData = _cache.Get(cacheKey) as JObject;
+
+            if (cachedData != null)
+            {
+                return new JsonNetResult(cachedData);
+            }
+
+            var resp = await _djMoneyApiRespository.GetGetCloseYearPerformance(fundID.ToUpper());
+            _cache.Add(cacheKey, resp, DateTimeOffset.Now.AddMinutes(30));
+            return new JsonNetResult(resp);
         }
     }
 }
