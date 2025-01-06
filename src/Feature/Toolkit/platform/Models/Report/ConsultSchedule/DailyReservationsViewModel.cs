4eac4847a676ea96122d8227e9b75bd59aac6b43
Mice.Lin <mice.lin@esi-tech.net>
2025-01-06 16:47:30 +0800

    #5310 查詢近30日預約人數跟清單的小工具

---
+++ src/Feature/Toolkit/platform/Models/Report/ConsultSchedule/DailyReservationsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Feature.Wealth.Toolkit.Models.Report
+{
+    public class DailyReservationsViewModel
+    {
+        public DateTime ScheduleDate { get; set; }
+        public int DailyReservations { get; set; }
+    }
+}
---
+++ src/Feature/Toolkit/platform/Models/Report/ConsultSchedule/StatusCountViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Feature.Wealth.Toolkit.Models.Report
+{
+    public class StatusCountViewModel
+    {
+        public int StatusCount { get; set; }
+        public string StatusName { get; set; }
+    }
+}
